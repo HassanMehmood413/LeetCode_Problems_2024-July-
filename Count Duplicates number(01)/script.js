a45104d962b7de6614ff7da30cb27a864dbd8c96
HassanMehmood413 <ihassan463m@gmail.com>
2024-07-09 22:16:45 +0500

    first program

---
+++ Count Duplicates number(01)/script.js
@@ -0,0 +1,37 @@
+let nums = [1,2,2,2,3,1]
+// var j = 0
+// let k = nums.sort()
+// if (k.length == 1) {
+//     console.log(nums[j]);
+// }
+// else {
+
+//     for (let i = 1; i < k.length; i++) {
+//         for (let j =  0; j < k.length; j++) {
+//             if(k.){
+//                 console.log(nums[i])
+//             }
+//         }
+
+//     }
+// }
+
+// let arr = nums.reduce((first,second)=>{
+//     if(nums.includes(first)){
+//         console.log('yes')
+//     }
+// })
+
+
+let a = [1,2,3]
+let b = [6,7,8]
+let c = a.reverse()
+console.log(c)
+let d = b.reverse()
+console.log(d)
+let sum  = c[0] + d[0]
+let p = []
+let k = p.push(sum)
+console.log(k)
+
+
