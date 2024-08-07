87cb832335df3ba75bf49885ad2cf9d1126e4678
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-07 07:36:15 +0500

    Time: 62 ms (75.61%), Space: 51.4 MB (74.39%) - LeetHub

--- 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.js
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.js
@@ -3,34 +3,48 @@
  * @return {number}
  */
 var numberOfSpecialChars = function (word) {
-    word = word.split('')
-    let map = new Map()
-    word.forEach(element => {
-        if (map.has(element)) {
-            map.set(element, map.get(element) + 1)
-        }
-        else {
-            map.set(element, 1)
-        }
-    });
-    let array = Array.from(map.keys())
+    // My Code
 
+    // word = word.split('')
+    // let map = new Map()
+    // word.forEach(element => {
+    //     if (map.has(element)) {
+    //         map.set(element, map.get(element) + 1)
+    //     }
+    //     else {
+    //         map.set(element, 1)
+    //     }
+    // });
+    // let array = Array.from(map.keys())
+
+    // let count = 0
+    // for (let i = 0; i < array.length; i++) {
+    //     for (let j = i + 1; j < array.length; j++) {
+    //         if (array[i] == array[i].toUpperCase()) {
+    //             a = array[i].toLowerCase()
+    //             if (a == array[j]) {
+    //                 count++
+    //             }
+    //         }
+    //         else if (array[i] == array[i].toLowerCase()) {
+    //             a = array[i].toUpperCase()
+    //             if (a == array[j]) {
+    //                 count++
+    //             }
+    //         }
+
+    //     }
+    // }
+    // return count
+
+
+    //Optimize Code:
+    let set = new Set([...word])
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
     let count = 0
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[i] == array[i].toUpperCase()) {
-                a = array[i].toLowerCase()
-                if (a == array[j]) {
-                    count++
-                }
-            }
-            else if (array[i] == array[i].toLowerCase()) {
-                a = array[i].toUpperCase()
-                if (a == array[j]) {
-                    count++
-                }
-            }
-
+    for (const letter of letters) {
+        if (set.has(letter) && set.has(letter.toUpperCase())) {
+            count = count + 1
         }
     }
     return count
