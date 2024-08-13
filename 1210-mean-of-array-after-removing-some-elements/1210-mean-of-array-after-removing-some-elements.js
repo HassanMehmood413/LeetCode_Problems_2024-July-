39fd67410121fbb9e2be19751842c2ebfea55762
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-13 12:47:19 +0500

    Time: 53 ms (76.32%), Space: 51.3 MB (16.67%) - LeetHub

--- 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.js
+++ 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.js
@@ -3,22 +3,33 @@
  * @return {number}
  */
 var trimMean = function (arr) {
+    // Both are same
+
+    // arr.sort((a, b) => a - b)
+    // length = arr.length * (5 / 100)
+    // let i = 0
+    // let j = 0
+    // while (i < length) {
+    //     arr.shift(j)
+    //     i++
+    // }
+    // let l = arr.length - 1
+    // while (j < length) {
+    //     arr.pop(l)
+    //     j++
+    // }
+    // let sum = 0
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum = arr[i] + sum
+    // }
+    // return sum / arr.length
+
     arr.sort((a, b) => a - b)
-    length = arr.length * (5 / 100)
-    let i = 0
-    let j = 0
-    while (i < length) {
-        arr.shift(j)
-        i++
-    }
-    let l = arr.length - 1
-    while (j < length) {
-        arr.pop(l)
-        j++
-    }
+    length = arr.length * 0.05
+
     let sum = 0
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = length; i < arr.length - length; i++) {
         sum = arr[i] + sum
     }
-    return sum / arr.length
+    return sum / (arr.length - length * 2)
 };
