ab36e1698ef0a0a9194c0673feee1a7eb86f077a
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-13 17:15:23 +0500

    Time: 62 ms (58.92%), Space: 52.4 MB (27.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,71 +4,47 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-    // let count = 0
-    // let i = 0
-    // let j = 0
-    // let arr = []
-    // while (i <= nums1.length - 1) {
+    // complexity : 0(n*m2)
 
-
-    //     if (nums1[i] == nums2[j]) {
-    //         let k = j
-    //         while (k <= nums2.length - 1) {
-    //             if (nums2[k + 1] > nums2[k]) {
-    //                 arr.push(nums2[k + 1])
-    //                 count = 0
-    //                 break
-    //             }
-    //             k++;
-    //             count++
+    // let indices = []
+    // for (let i = 0; i < nums1.length; i++) {
+    //     let index = nums2.indexOf(nums1[i])
+    //     indices.push(index)
+    // }
+    // let b;
+    // let max = -1
+    // for (let i = 0; i < indices.length; i++) {
+    //     let a = indices[i]
+    //     let comp = nums2[a]
+    //     for (let j = a; j < nums2.length; j++) {
+    //         b = nums2[j]
+    //         if (b > comp) {
+    //             max = b
+    //             break
     //         }
-
+    //         else {
+    //             max = -1
+    //         }
     //     }
-    //     if (count > 0) {
-    //         arr.push(-1)
-    //         count = 0
-    //     }
-    //     if (j == nums2.length - 1) {
-    //         i++
-    //     }
-
-    //     if (j == nums2.length - 1) {
-    //         j = 0
+    //     if (max < comp) {
+    //         nums1[i] = -1
     //     }
     //     else {
-    //         j++
+    //         nums1[i] = max
     //     }
-
     // }
-    // return arr
+    // return nums1
 
-    let indices = []
-    for (let i = 0; i < nums1.length; i++) {
-        let index = nums2.indexOf(nums1[i])
-        indices.push(index)
+    // Efficient Complexity: 0(n * m):
+    let map = new Map()
+    let stack = []
+    for (let num of nums2) {
+        while (stack.length > 0 && stack[stack.length - 1]<num){
+            map.set(stack.pop(),num)
+        }
+        stack.push(num)
     }
-    let b;
-    let max = -1
-    for (let i = 0; i < indices.length; i++) {
-        let a = indices[i]
-        let comp = nums2[a]
-        for (let j = a; j < nums2.length; j++) {
-            b = nums2[j]
-            if (b > comp) {
-                max = b
-                break
-            }
-            else {
-                max = -1
-            }
-        }
-        if (max < comp) {
-            nums1[i] = -1
-        }
-        else {
-            nums1[i] = max
-        }
-    }
-    return nums1
+    return nums1.map((num)=>map.get(num)||-1)
+
 
 };
