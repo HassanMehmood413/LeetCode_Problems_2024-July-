585104167daf70483200b065ef737b0685457b8a
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-16 08:33:50 +0500

    Time: 58 ms (43.97%), Space: 51.3 MB (7.31%) - LeetHub

---
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function (root) {
+    let queue = [root]
+    let val = root.val
+    sum = 0
+    while (queue.length > 0) {
+        let length = queue.length
+        for (let i = 0; i < length; i++) {
+            let first = queue.shift()
+            if(first.left != null){
+                sum = sum + first.left.val
+            }
+            if(first.right != null){
+                sum = sum + first.right.val
+            }
+        }
+    }
+    if(sum == val){
+        return true
+    }
+    else{
+        return false
+    }
+};
