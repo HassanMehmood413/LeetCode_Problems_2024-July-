89f645daeb1356c5ccf05704ef10b0912418b116
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-19 17:40:07 +0500

    Time: 66 ms (29.96%), Space: 52.4 MB (97.77%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+    let sum = nums[0]
+    let length = 1
+    let max_l = Infinity
+    let k = 0
+    for (let i = 1; i <= nums.length; i++) {
+        while (sum >= target) {
+            if (length < max_l) {
+                max_l = length
+            }
+            sum = sum - nums[k]
+            k++
+            length--
+        }
+        sum = sum + nums[i]
+        length++
+    }
+    if (max_l == Infinity) {
+        max_l = 0
+    }
+    return max_l
+};
