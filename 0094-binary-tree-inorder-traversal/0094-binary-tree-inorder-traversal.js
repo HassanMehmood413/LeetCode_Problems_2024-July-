1626ce3be590d9181165a774a4c70bd19a883494
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-21 16:30:31 +0500

    Time: 57 ms (26.23%), Space: 49 MB (49.92%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    // using DFS
+    let stack = []
+    let inorder = []
+    while (root != null || stack.length != 0) {
+        if (root) {
+            stack.push(root)
+            root = root.left
+        }
+        else {
+            root = stack.pop()
+            inorder.push(root.val)
+            root = root.right
+        }
+    }
+    return inorder
+
+};
