3a2db574397c2832b72cb2a82c2395bc85175a44
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-21 18:17:20 +0500

    Time: 192 ms (53.97%), Space: 88.9 MB (15.12%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -11,23 +11,38 @@
  * @return {number}
  */
 var minDepth = function (root) {
-    let min = 1
-    if (!root) return []
-    let queue = [root]
-    while (queue.length > 0) {
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            if (first.left == null && first.right == null) {
-                return min
-            }
-            if (first.left != null) {
-                queue.push(first.left ? first.left : null)
-            }
-            if (first.right != null) {
-                queue.push(first.right ? first.right : null)
-            }
+    // USING BFS
+    // let min = 1
+    // if (!root) return []
+    // let queue = [root]
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         if (first.left == null && first.right == null) {
+    //             return min
+    //         }
+    //         if (first.left != null) {
+    //             queue.push(first.left ? first.left : null)
+    //         }
+    //         if (first.right != null) {
+    //             queue.push(first.right ? first.right : null)
+    //         }
+    //     }
+    //     min++
+    // }
+
+    // USING RECURSION
+    // var again = function (root) {
+        if (!root) {
+            return 0
         }
-        min++
-    }
+        if (root.left == null) {
+            return minDepth(root.right) + 1
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+    // }
 };
