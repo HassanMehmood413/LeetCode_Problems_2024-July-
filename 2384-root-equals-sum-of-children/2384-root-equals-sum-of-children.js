b4e8c448549597b7ac2a097f575382a5886845c9
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-21 18:28:03 +0500

    Time: 45 ms (95.6%), Space: 51 MB (35.62%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.js
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.js
@@ -11,25 +11,31 @@
  * @return {boolean}
  */
 var checkTree = function (root) {
-    let queue = [root]
-    let val = root.val
-    sum = 0
-    while (queue.length > 0) {
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            if(first.left != null){
-                sum = sum + first.left.val
-            }
-            if(first.right != null){
-                sum = sum + first.right.val
-            }
-        }
-    }
-    if(sum == val){
-        return true
-    }
-    else{
-        return false
-    }
+    // Using BFS
+
+    // let queue = [root]
+    // let val = root.val
+    // sum = 0
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         if(first.left != null){
+    //             sum = sum + first.left.val
+    //         }
+    //         if(first.right != null){
+    //             sum = sum + first.right.val
+    //         }
+    //     }
+    // }
+    // if(sum == val){
+    //     return true
+    // }
+    // else{
+    //     return false
+    // }
+
+    // Using Recursion
+    if(!root)return 0
+    return root.val === root.left.val + root.right.val
 };
