17816f8ad5d43222ead14a1e652d4c926a0d10d3
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-21 18:02:59 +0500

    Time: 53 ms (49.98%), Space: 49.2 MB (34.86%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -12,19 +12,30 @@
  */
 var inorderTraversal = function (root) {
     // using DFS
-    let stack = []
-    let inorder = []
-    while (root != null || stack.length != 0) {
+    // let stack = []
+    // let inorder = []
+    // while (root != null || stack.length != 0) {
+    //     if (root) {
+    //         stack.push(root)
+    //         root = root.left
+    //     }
+    //     else {
+    //         root = stack.pop()
+    //         inorder.push(root.val)
+    //         root = root.right
+    //     }
+    // }
+    // return inorder
+    let res = []
+    var again = function (root) {
         if (root) {
-            stack.push(root)
-            root = root.left
-        }
-        else {
-            root = stack.pop()
-            inorder.push(root.val)
-            root = root.right
+            again(root.left)
+            res.push(root.val)
+            again(root.right)
         }
     }
-    return inorder
+    again(root)
+    return res
+
 
 };
