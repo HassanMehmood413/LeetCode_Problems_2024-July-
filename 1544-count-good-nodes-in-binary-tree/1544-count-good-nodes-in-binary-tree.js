6f3c7caa042a58695f874e60cbc23bdaee5583e3
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-22 17:01:45 +0500

    Time: 146 ms (11.1%), Space: 71.2 MB (17.68%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
@@ -11,28 +11,64 @@
  * @return {number}
  */
 var goodNodes = function (root) {
-    if (root === null) { return 0 }
-    let queue = [{ node: root, maxsofar: root.val }]
+    // if (root === null) { return 0 }
+    // let queue = [{ node: root, maxsofar: root.val }]
+    // let count = 0
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++) {
+
+    //         let { node, maxsofar } = queue.shift()
+
+    //         if (node.val >= maxsofar) {
+    //             count++
+    //         }
+    //         newmax = Math.max(maxsofar, node.val)
+    //         if (node.left != null) {
+    //             queue.push({ node: node.left, maxsofar: newmax })
+    //         }
+    //         if (node.right != null) {
+    //             queue.push({ node: node.right, maxsofar: newmax })
+    //         }
+    //     }
+    // }
+    // return count
+
+    // USING RECURSION
+    // let count = 0
+    // function dfs(node, max) {
+    //     if (!node) {
+    //         return 
+    //     }
+    //     if (node.val >= max) {
+    //         count++
+    //     }
+    //     dfs(node.left, Math.max(max, node.val))
+    //     dfs(node.right, Math.max(max, node.val))
+    // }
+
+    // dfs(root, root.val)
+    // return count
+
+    //Using DFS
+    let stack = []
+    stack.push([root, -Infinity])
     let count = 0
-    while (queue.length > 0) {
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-
-            let { node, maxsofar } = queue.shift()
-
-            if (node.val >= maxsofar) {
-                count++
-            }
-            newmax = Math.max(maxsofar, node.val)
-            if (node.left != null) {
-                queue.push({ node: node.left, maxsofar: newmax })
-            }
-            if (node.right != null) {
-                queue.push({ node: node.right, maxsofar: newmax })
-            }
+    let value = root.val
+    while (stack.length > 0) {
+        let [node, max] = stack.pop()
+        const { left, right, val } = node
+        if (max <= val) {
+            count++
+            max = val
+        }
+        if (left) {
+            stack.push([left,max])
+        }
+        if (right) {
+            stack.push([right,max])
         }
     }
     return count
-
 
 }
