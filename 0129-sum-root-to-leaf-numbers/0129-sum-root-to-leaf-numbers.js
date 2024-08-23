7cbba30854bfa19b82e6d4ae4e77550bd8a6281a
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-23 09:11:27 +0500

    Time: 59 ms (34.87%), Space: 50 MB (37.63%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -11,26 +11,60 @@
  * @return {number}
  */
 var sumNumbers = function (root) {
-    let stack = [[root, 0]]
-    let sum = 0
-    while (stack.length > 0) {
-        let length = stack.length
-        for (let i = 0; i < length; i++) {
-            let [first, currentnum] = stack.pop()
-            currentnum = currentnum * 10 + first.val
-            if (first.left == null && first.right == null) {
-                sum = sum + currentnum
+    // let stack = [[root, 0]]
+    // let sum = 0
+    // while (stack.length > 0) {
+    //     let length = stack.length
+    //     for (let i = 0; i < length; i++) {
+    //         let [first, currentnum] = stack.pop()
+    //         currentnum = currentnum * 10 + first.val
+    //         if (first.left == null && first.right == null) {
+    //             sum = sum + currentnum
 
-            }
-            if (first.left != null) {
-                stack.push([first.left, currentnum])
-            }
-            if (first.right != null) {
-                stack.push([first.right, currentnum])
-            }
+    //         }
+    //         if (first.left != null) {
+    //             stack.push([first.left, currentnum])
+    //         }
+    //         if (first.right != null) {
+    //             stack.push([first.right, currentnum])
+    //         }
+    //     }
+    // }
+    // return sum
+
+
+    // Using BFS Approach
+    // let queue = [[root, 0]]
+    // let sum = 0
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++) {
+    //         let [first, currentnum] = queue.shift()
+    //         currentnum = currentnum * 10 + first.val
+    //         if (first.left == null && first.right == null) {
+    //             sum = sum + currentnum
+    //         }
+    //         if (first.left != null) {
+    //             queue.push([first.left, currentnum])
+    //         }
+    //         if (first.right != null) {
+    //             queue.push([first.right, currentnum])
+    //         }
+
+    //     }
+    // }
+    // return sum
+
+    // Using Recursion
+    let result = function (root, currentnum) {
+        if (!root) return 0;
+        currentnum = currentnum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return currentnum;
         }
+        let leftsum = result(root.left, currentnum);
+        let rightsum = result(root.right, currentnum);
+        return leftsum + rightsum;
     }
-    return sum
-
-
+    return result(root,0)
 };
