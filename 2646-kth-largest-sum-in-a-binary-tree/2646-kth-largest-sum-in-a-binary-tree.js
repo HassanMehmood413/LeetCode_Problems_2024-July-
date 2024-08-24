bb04f23840c7555c381003eb68197888883dc710
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-24 07:35:34 +0500

    Time: 312 ms (71.05%), Space: 94.8 MB (55.26%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.js
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.js
@@ -12,28 +12,68 @@
  * @return {number}
  */
 var kthLargestLevelSum = function (root, k) {
-    let queue = [root]
-    let arr = []
-    while (queue.length > 0) {
-        let length = queue.length
-        let sum = 0
+    // Using BFS
+
+    // let queue = [root]
+    // let arr = []
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     let sum = 0
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         sum = sum + first.val
+    //         if (first.left != null) {
+    //             queue.push(first.left)
+    //         }
+    //         if (first.right != null) {
+    //             queue.push(first.right)
+    //         }
+    //     }
+    //     arr.push(sum)
+    // }
+    // arr.sort((a,b)=>b-a)
+    // if(arr.length >= k){
+    //     return arr[k-1]
+    // }
+    // else{
+    //     return -1
+    // }
+
+    // Using DFS
+    let stack = [root];
+    let arr = [];
+
+    while (stack.length > 0) {
+        let length = stack.length;
+        let sum = 0;
+
+        // Temporary array to hold nodes for the next level
+        let tempStack = [];
+
         for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            sum = sum + first.val
-            if (first.left != null) {
-                queue.push(first.left)
+            let node = stack.pop(); 
+            sum += node.val;
+            if (node.left) {
+                tempStack.push(node.left);
             }
-            if (first.right != null) {
-                queue.push(first.right)
+            if (node.right) {
+                tempStack.push(node.right);
             }
         }
-        arr.push(sum)
+
+        arr.push(sum);
+
+        // After processing the current level, push all nodes in tempStack
+        // back to the main stack in the order they should be processed
+        stack = tempStack;
     }
-    arr.sort((a,b)=>b-a)
-    if(arr.length >= k){
-        return arr[k-1]
+
+    arr.sort((a, b) => b - a);
+    if (arr.length >= k) {
+        return arr[k - 1]
     }
-    else{
+    else {
         return -1
     }
+
 };
