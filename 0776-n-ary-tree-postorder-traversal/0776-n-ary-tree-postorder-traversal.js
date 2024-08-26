39eaa7720b7aabe74fa483a70a00f7978917c2ae
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-26 09:14:12 +0500

    Time: 82 ms (19.61%), Space: 53.6 MB (49.02%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.js
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.js
@@ -11,17 +11,34 @@
  * @return {number[]}
  */
 var postorder = function (root) {
+    // Using DFS
+    // if (!root) return []
+    // let stack = [root]
+    // let arr = []
+    // let second = []
+    // while (stack.length > 0) {
+    //     let length = stack.length
+    //     for (let i = 0; i < length; i++) {
+    //         let first = stack.pop()
+    //         arr.unshift(first.val)
+    //         for (let j of first.children) {
+    //             stack.push(j)
+    //         }
+    //     }
+    // }
+    // return arr
+    // Using BFS
     if (!root) return []
-    let stack = [root]
+    let queue = [root]
     let arr = []
     let second = []
-    while (stack.length > 0) {
-        let length = stack.length
+    while (queue.length > 0) {
+        let length = queue.length
         for (let i = 0; i < length; i++) {
-            let first = stack.pop()
+            let first = queue.pop()
             arr.unshift(first.val)
             for (let j of first.children) {
-                stack.push(j)
+                queue.push(j)
             }
         }
     }
