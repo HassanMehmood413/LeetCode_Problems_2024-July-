19423eab8abc86869368043fcf0820dd16f04ede
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-26 07:57:51 +0500

    Time: 62 ms (100%), Space: 54 MB (70.97%) - LeetHub

--- 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.js
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.js
@@ -11,20 +11,36 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    if (!root) return []
-    let queue = [root]
+    // Recomended solution is BFS
+    // Using BFS
+    // if (!root) return []
+    // let queue = [root]
+    // let result = []
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     arr = []
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         arr.push(first.val)
+    //         for (let j of first.children) {
+    //             queue.push(j)
+    //         }
+    //     }
+    //     result.push(arr)
+    // }
+    // return result
+
     let result = []
-    while (queue.length > 0) {
-        let length = queue.length
-        arr = []
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            arr.push(first.val)
-            for (let j of first.children) {
-                queue.push(j)
-            }
+    var again = function (root, level) {
+        if (!root) return []
+        if (result.length == level) {
+            result.push([]) // This is for new block of array
         }
-        result.push(arr)
+        result[level].push(root.val)
+        for (let j of root.children) {
+            again(j, level + 1)
+        }
     }
+    again(root, 0)
     return result
 };
