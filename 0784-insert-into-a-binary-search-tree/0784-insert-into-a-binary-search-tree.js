b7d1e703d6ec1635b8424687dd185132110ec5c7
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-26 07:05:49 +0500

    Time: 104 ms (17.13%), Space: 58.9 MB (36.54%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.js
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.js
@@ -12,19 +12,47 @@
  * @return {TreeNode}
  */
 var insertIntoBST = function (root, val) {
-    let queue = [root]
+    // let queue = [root]
+    // if(!root) return new TreeNode(val)
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++){
+    //         let first = queue.shift()
+    //         if(val < first.val){
+    //             if(first.left == null){
+    //                 first.left = new TreeNode(val)
+    //                 break
+    //             }
+    //             else{
+    //                 queue.push(first.left)
+    //             }
+    //         }
+    //         else{
+    //             if(first.right == null){
+    //                 first.right = new TreeNode(val)
+    //                 break
+    //             }
+    //             else{
+    //                 queue.push(first.right)
+    //             }
+    //         }
+    //     }
+    // }
+    // return root
+
+    let stack = [root]
     if(!root) return new TreeNode(val)
-    while (queue.length > 0) {
-        let length = queue.length
+    while (stack.length > 0) {
+        let length = stack.length
         for (let i = 0; i < length; i++){
-            let first = queue.shift()
+            let first = stack.pop()
             if(val < first.val){
                 if(first.left == null){
                     first.left = new TreeNode(val)
                     break
                 }
                 else{
-                    queue.push(first.left)
+                    stack.push(first.left)
                 }
             }
             else{
@@ -33,7 +61,7 @@ var insertIntoBST = function (root, val) {
                     break
                 }
                 else{
-                    queue.push(first.right)
+                    stack.push(first.right)
                 }
             }
         }
