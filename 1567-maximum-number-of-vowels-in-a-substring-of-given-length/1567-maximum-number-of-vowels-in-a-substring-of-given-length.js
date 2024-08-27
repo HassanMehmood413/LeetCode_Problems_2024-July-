16bd670ebdedba772b75f13bba02a1b464981fbe
Hassan Mehmood <ihassan463m@gmail.com>
2024-08-27 09:56:21 +0500

    Time: 394 ms (5.75%), Space: 58.8 MB (9.28%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -4,22 +4,44 @@
  * @return {number}
  */
 var maxVowels = function (s, k) {
+    // s = s.split('')
+    // let vowels = 'aeiou'
+    // let str = []
+    // let count = 0
+    // for (let i = 0; i < k; i++) {
+    //     str.push(s[i])
+    //     if (vowels.includes(s[i])) {
+    //         count++
+    //     }
+    // }
+    // let max = count
+    // for (let i = k; i < s.length; i++) {
+    //     str.push(s[i])
+    //     let a = str.shift()
+    //     if (vowels.includes(a)) {
+    //         count--
+    //     }
+    //     if (vowels.includes(s[i])) {
+    //         count++
+    //     }
+    //     max = Math.max(count, max)
+
+    // }
+    // return max
+
+
     s = s.split('')
     let vowels = 'aeiou'
     let str = []
     let count = 0
-    for (let i = 0; i < k; i++) {
-        str.push(s[i])
-        if (vowels.includes(s[i])) {
-            count++
-        }
-    }
     let max = count
-    for (let i = k; i < s.length; i++) {
+    for (let i = 0; i < s.length; i++) {
         str.push(s[i])
-        let a = str.shift()
-        if (vowels.includes(a)) {
-            count--
+        if (str.length > k) {
+            let a = str.shift()
+            if (vowels.includes(a)) {
+                count--
+            }
         }
         if (vowels.includes(s[i])) {
             count++
