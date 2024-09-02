5d98355d36829c46812843a37aed401e1f51becb
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-02 18:38:41 +0500

    Time: 77 ms (56.67%), Space: 54.3 MB (77%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,24 +11,40 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-    if (!root) return []
-    let queue = [root]
+    // if (!root) return []
+    // let queue = [root]
+    // let arr = []
+    // let comb = []
+    // while (queue.length > 0) {
+    //     let length = queue.length
+    //     comb = []
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         comb.push(first.val)
+    //         if (first.left != null) {
+    //             queue.push(first.left)
+    //         }
+    //         if (first.right != null) {
+    //             queue.push(first.right)
+    //         }
+    //     }
+    //     arr.push(comb)
+    // }
+    // return arr
+
+
+    // Using recursion
     let arr = []
-    let comb = []
-    while (queue.length > 0) {
-        let length = queue.length
-        comb = []
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            comb.push(first.val)
-            if (first.left != null) {
-                queue.push(first.left)
-            }
-            if (first.right != null) {
-                queue.push(first.right)
-            }
-        }
-        arr.push(comb)
+    var again = function (root, arr, level) {
+        if (!root) return []
+        if(arr.length == level) arr.push([])
+        arr[level].push(root.val)
+
+
+        again(root.left, arr, level + 1)
+        again(root.right, arr, level + 1)
+
     }
+    again(root, arr, 0)
     return arr
 };
