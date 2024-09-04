f6c2c9028fb79c3051b1b4ea9a7190153d3551a0
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-04 13:03:07 +0500

    Time: 55 ms (86.52%), Space: 53.6 MB (27.42%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -11,14 +11,28 @@
  * @return {boolean}
  */
 var isValidBST = function (root) {
-    function valid(root, low, right) {
-        if (root == null) {
-            return true
-        }
-        if (!(low < root.val && root.val < right)) {
-            return false
-        }
-        return valid(root.left, low, root.val) && valid(root.right, root.val, right)
+    // function valid(root, low, right) {
+    //     if (root == null) {
+    //         return true
+    //     }
+    //     if (!(low < root.val && root.val < right)) {
+    //         return false
+    //     }
+    //     return valid(root.left, low, root.val) && valid(root.right, root.val, right)
+    // }
+    // return valid(root, -Infinity, Infinity)
+
+
+    function again(root,min,max){
+        if(!root) return true
+        if((min != null && min >=root.val) || (max != null && max <=root.val)) return false
+
+
+
+        return again(root.left , min,root.val) && again(root.right,root.val,max)
     }
-    return valid(root, -Infinity, Infinity)
+    return again(root,null,null)
+
+
+
 };
