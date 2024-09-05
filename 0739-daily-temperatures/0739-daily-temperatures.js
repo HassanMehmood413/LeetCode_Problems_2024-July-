4189762a5e1d253146ab4cbd2fc15bd6194c69a7
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-05 13:18:47 +0500

    Time: 192 ms (93.55%), Space: 73.7 MB (46.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,6 +3,7 @@
  * @return {number[]}
  */
 var dailyTemperatures = function (temperatures) {
+    // Using Only Index
     let stack = []
     let output = new Array(temperatures.length).fill(0)
     stack.push(0)
@@ -16,5 +17,22 @@ var dailyTemperatures = function (temperatures) {
     }
 
     return output
+
+    // Using Both [index and Value]
+    // let stack = []
+    // let output = new Array(temperatures.length).fill(0)
+    // stack.push([temperatures[0], 0])
+    // for (let i = 1; i < temperatures.length; i++) {
+    //     while (stack.length > 0 && stack[stack.length - 1][0] < temperatures[i]) {
+    //         index = stack.pop()
+    //         let a = index[1]
+    //         output[a] = i - a
+    //         console.log(index[1])
+    //     }
+    //     stack.push([temperatures[i], i])
+
+    // }
+
+    // return output
 
 };
