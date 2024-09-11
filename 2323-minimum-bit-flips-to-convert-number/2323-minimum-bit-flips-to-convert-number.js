d5d2d2caccf158381f38ab85434d13cedd5a1b16
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-11 07:07:12 +0500

    Time: 52 ms (63.77%), Space: 49 MB (39.86%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} start
+ * @param {number} goal
+ * @return {number}
+ */
+var minBitFlips = function (start, goal) {
+
+    start = BigInt(start).toString(2)
+    goal = BigInt(goal).toString(2)
+    if (goal.length < start.length) {
+        while (goal.length != start.length) {
+
+            goal = goal.split('')
+            goal.unshift('0')
+            goal = goal.join("")
+        }
+    }
+    else if (start.length < goal.length) {
+        while (start.length != goal.length) {
+
+            start = start.split('')
+            start.unshift('0')
+            start = start.join("")
+        }
+    }
+    let count = 0
+    for (let i = 0; i < start.length; i++) {
+        if (start[i] != goal[i]) {
+            count++
+        }
+    }
+    return count
+};
