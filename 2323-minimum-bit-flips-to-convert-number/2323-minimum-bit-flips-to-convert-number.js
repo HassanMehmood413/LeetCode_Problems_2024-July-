83ccc1b653398161035bb7b220d67b3693921f0d
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-11 09:37:19 +0500

    Time: 54 ms (53.62%), Space: 48.7 MB (81.88%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.js
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.js
@@ -5,29 +5,35 @@
  */
 var minBitFlips = function (start, goal) {
 
-    start = BigInt(start).toString(2)
-    goal = BigInt(goal).toString(2)
-    if (goal.length < start.length) {
-        while (goal.length != start.length) {
+    // start = BigInt(start).toString(2)
+    // goal = BigInt(goal).toString(2)
+    // if (goal.length < start.length) {
+    //     while (goal.length != start.length) {
 
-            goal = goal.split('')
-            goal.unshift('0')
-            goal = goal.join("")
-        }
-    }
-    else if (start.length < goal.length) {
-        while (start.length != goal.length) {
+    //         goal = goal.split('')
+    //         goal.unshift('0')
+    //         goal = goal.join("")
+    //     }
+    // }
+    // else if (start.length < goal.length) {
+    //     while (start.length != goal.length) {
 
-            start = start.split('')
-            start.unshift('0')
-            start = start.join("")
-        }
-    }
-    let count = 0
-    for (let i = 0; i < start.length; i++) {
-        if (start[i] != goal[i]) {
-            count++
-        }
-    }
-    return count
+    //         start = start.split('')
+    //         start.unshift('0')
+    //         start = start.join("")
+    //     }
+    // }
+    // let count = 0
+    // for (let i = 0; i < start.length; i++) {
+    //     if (start[i] != goal[i]) {
+    //         count++
+    //     }
+    // }
+    // return count
+
+
+    // Short method
+    let xgate = start ^ goal
+    let binary = xgate.toString(2)
+    return binary.split('1').length-1
 };
