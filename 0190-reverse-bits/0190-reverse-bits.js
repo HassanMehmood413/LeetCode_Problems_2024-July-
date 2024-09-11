b40c95485a0d50e9293168c2128aad9eea044fcf
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-11 18:15:09 +0500

    Time: 62 ms (41.64%), Space: 51.5 MB (12.66%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -5,6 +5,7 @@
 var reverseBits = function (n) {
     n = n.toString(2).padStart(32, '0');
     n = n.split('').reverse().join('')
+    // we use parseInt to convert binary numbers into integers
     let b = parseInt(n, 2)
     return b
 };
