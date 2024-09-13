e126b4d1967edd2c40a66189642a131250a0f2a7
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-13 07:53:10 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,26 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (arr) {
-    // let maxEndingHere = arr[0];
-    // let maxSoFar = arr[0];
-
-    // for (let i = 1; i < arr.length; i++) {
-    //     maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-    //     maxSoFar = Math.max(maxSoFar, maxEndingHere);
-    // }
-
-    // return maxSoFar;
+var maxSubArray = function (nums) {
+    let max = nums[0]
     let sum = 0
-    let max = arr[0]
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i]
-        if(sum > max){
-            max = sum
-        }
-        if(sum < 0){
+    for (let i = 0; i < nums.length; i++) {
+        sum = sum + nums[i]
+        if (sum < 0) {
             sum = 0
         }
+        if (sum > max) {
+            max = sum
+
+        }
     }
-    return max
+    if (max < 0) {
+        return Math.min(...nums)
+    }
+    else {
+        return max
+    }
 };
