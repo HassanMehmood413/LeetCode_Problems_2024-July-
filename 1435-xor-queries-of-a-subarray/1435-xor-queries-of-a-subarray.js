2cfbe6b4c762168081f5eb7579dadba761e6cc00
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-13 07:33:13 +0500

    Time: 105 ms (33.33%), Space: 64.1 MB (46.15%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.js
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.js
@@ -4,16 +4,30 @@
  * @return {number[]}
  */
 var xorQueries = function (arr, queries) {
-    let first = 0
-    let second = 1
+    // Complexity : 0(n2)
+
+    // let first = 0
+    // let second = 1
+    // let arr2 = []
+    // let temp = 0
+    // for (let i = 0; i < queries.length; i++) {
+    //     for (let j = queries[i][first]; j <= queries[i][second]; j++) {
+    //         temp = arr[j] ^ temp
+    //     }
+    //     arr2.push(temp)
+    //     temp = 0
+    // }
+    // return arr2
+
+    // Complexity : 0(n)
     let arr2 = []
-    let temp = 0
-    for (let i = 0; i < queries.length; i++) {
-        for (let j = queries[i][first]; j <= queries[i][second]; j++) {
-            temp = arr[j] ^ temp
-        }
-        arr2.push(temp)
-        temp = 0
+    let temp = [0]
+    for (let i = 1; i <= arr.length; i++) {
+        temp[i] = temp[i - 1] ^ arr[i - 1]
+    }
+    console.log(temp)
+    for (const [start, end] of queries) {
+        arr2.push(temp[start] ^ temp[end + 1])
     }
     return arr2
 }
