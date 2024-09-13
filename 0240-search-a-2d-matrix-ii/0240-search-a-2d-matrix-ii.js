0a2b3aa7c5b2066b25cbba07030a5715e682924b
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-13 08:35:50 +0500

    Time: 322 ms (47.96%), Space: 52.5 MB (69.7%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    // let arr = matrix.flat()
+    // let left = 0
+    // let right = arr.length - 1
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2)
+    //     if (arr[mid] == target) {
+    //         return true
+    //     }
+    //     else if (arr[mid] > target) {
+    //         right = mid - 1
+    //     }
+    //     else {
+    //         left = mid + 1
+    //     }
+    // }
+    // return false
+    if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return false
+    let col = matrix[0].length - 1
+    let row = 0
+    while (col >= 0 && row <= matrix.length - 1) {
+        let el = matrix[row][col]
+        if (el == target) {
+            return true
+        }
+        else if (el > target) {
+            col--
+        }
+        else if(el < target){
+            row++
+        }
+    }
+    return false
+};
