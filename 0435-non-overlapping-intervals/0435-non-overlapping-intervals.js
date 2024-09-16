f264d5609c785692eb2a0104c0846f80ed290d94
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-16 11:18:16 +0500

    Time: 181 ms (94.25%), Space: 79.7 MB (81.55%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let count = 0
+    let max = intervals[0][1]
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] < max) {
+            count++
+        }
+
+        else {
+            max = intervals[i][1]
+        }
+    }
+    return count
+};
