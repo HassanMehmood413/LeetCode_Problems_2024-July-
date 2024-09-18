f5d38b0f76c2403bb29a627f4fbb533899c643e1
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-18 06:59:43 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.js
@@ -11,64 +11,24 @@
  * @return {number}
  */
 var goodNodes = function (root) {
-    // if (root === null) { return 0 }
-    // let queue = [{ node: root, maxsofar: root.val }]
-    // let count = 0
-    // while (queue.length > 0) {
-    //     let length = queue.length
-    //     for (let i = 0; i < length; i++) {
-
-    //         let { node, maxsofar } = queue.shift()
-
-    //         if (node.val >= maxsofar) {
-    //             count++
-    //         }
-    //         newmax = Math.max(maxsofar, node.val)
-    //         if (node.left != null) {
-    //             queue.push({ node: node.left, maxsofar: newmax })
-    //         }
-    //         if (node.right != null) {
-    //             queue.push({ node: node.right, maxsofar: newmax })
-    //         }
-    //     }
-    // }
-    // return count
-
-    // USING RECURSION
-    // let count = 0
-    // function dfs(node, max) {
-    //     if (!node) {
-    //         return 
-    //     }
-    //     if (node.val >= max) {
-    //         count++
-    //     }
-    //     dfs(node.left, Math.max(max, node.val))
-    //     dfs(node.right, Math.max(max, node.val))
-    // }
-
-    // dfs(root, root.val)
-    // return count
-
-    //Using DFS
-    let stack = []
-    stack.push([root, -Infinity])
+    let stack = [{ node: root, max: root.val }]
     let count = 0
-    let value = root.val
+    let max = root.val
     while (stack.length > 0) {
-        let [node, max] = stack.pop()
-        const { left, right, val } = node
-        if (max <= val) {
-            count++
-            max = val
-        }
-        if (left) {
-            stack.push([left,max])
-        }
-        if (right) {
-            stack.push([right,max])
-        }
+        // let length = stack.length
+        // for (let i = 0; i < length; i++) {
+            let { node, max } = stack.pop()
+            if (node.val >= max) {
+                max = node.val
+                count++
+            }
+            if (node.left != null) {
+                stack.push({ node: node.left, max: node.val })
+            }
+            if (node.right != null) {
+                stack.push({ node: node.right, max: node.val })
+            }
+        // }
     }
     return count
-
-}
+};
