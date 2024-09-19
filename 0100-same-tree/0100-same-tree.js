a617e86a2848fbc176628446bd265c47122bf728
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-19 09:31:44 +0500

    Time: 56 ms (36.7%), Space: 49.3 MB (43.45%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -11,10 +11,29 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    if(!p && !q) return true
-    if(p && q && p.val == q.val){
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
-    }
-    return false
-};
+var isSameTree = function (p, q) {
+    // Using Recursion
+    // if (!p && !q) return true
+    // if (p && q && p.val == q.val) {
+    //     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    // }
+    // return false
+
+    if (!p && !q) return true
+    if (!p || !q) return false
+    let queue = [p]
+    let queue2 = [q]
+    while (queue.length > 0 && queue2.length > 0) {
+        let first = queue.shift()
+        let second = queue2.shift()
+        if (!first && !second) continue
+        if (!first || !second) return false
+        if (first.val != second.val) return false
+        queue.push(first.left)
+        queue.push(first.right)
+        queue2.push(second.left)
+        queue2.push(second.right)
+    } return queue.length == 0 && queue2.length == 0
+
+
+};   
