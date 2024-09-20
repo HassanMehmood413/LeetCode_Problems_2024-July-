8887a93aa02cde7f56c6ab258bde9d18c80d34ae
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-20 08:41:01 +0500

    Time: 83 ms (18.01%), Space: 57 MB (8.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -4,13 +4,11 @@
  */
 var canJump = function (nums) {
     let maxjump = 0
-    let min = Math.min(...nums)
-    if (min > 0) return true
-    else {
-        for (let i = 0; i < nums.length; i++) {
-            maxjump = Math.max(maxjump, i + nums[i])
-            if (maxjump <= i + 1) break
-        }
-        return maxjump >= nums.length - 1
+    a = Math.min(nums)
+    if (a > 0) return true
+    for (let i = 0; i < nums.length; i++) {
+        maxjump = Math.max(maxjump, i + nums[i])
+        if (maxjump < i + 1) break
     }
+    return maxjump >= nums.length - 1
 };
