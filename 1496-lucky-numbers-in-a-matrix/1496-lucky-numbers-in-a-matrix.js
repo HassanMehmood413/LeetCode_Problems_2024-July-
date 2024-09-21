18f08e5317d73028f2cf9acc3b754ea80eb927d3
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-21 14:47:22 +0500

    Time: 49 ms (92.9%), Space: 51 MB (94.93%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.js
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.js
@@ -3,23 +3,34 @@
  * @return {number[]}
  */
 var luckyNumbers = function (matrix) {
-    let arr = []
-    for (let i = 0; i < matrix.length; i++) {
-        let a = Math.min(...matrix[i])
-        arr.push(a)
-    }
-    for (let i = 0; i < matrix.length; i++) {
-        let a = arr.shift()
-        let index = matrix[i].indexOf(a)
-        let count = 0
-        for (let j = 0; j < matrix.length; j++) {
-            if (a >= matrix[j][index]) {
-                count++
-            }
-        }
-        if (count == matrix.length) {
-            return [a]
-        }
+    // my method
+
+    // let arr = []
+    // for (let i = 0; i < matrix.length; i++) {
+    //     let a = Math.min(...matrix[i])
+    //     arr.push(a)
+    // }
+    // for (let i = 0; i < matrix.length; i++) {
+    //     let a = arr.shift()
+    //     let index = matrix[i].indexOf(a)
+    //     let count = 0
+    //     for (let j = 0; j < matrix.length; j++) {
+    //         if (a >= matrix[j][index]) {
+    //             count++
+    //         }
+    //     }
+    //     if (count == matrix.length) {
+    //         return [a]
+    //     }
+    // }
+    // return []
+
+    // kisi ajnabi ka method
+    for (let i = 0; i < matrix.length; i++){
+        let arr = matrix[i]
+        let min = Math.min(...arr)
+        let index = matrix[i].indexOf(min)
+        if(matrix.every(element => element[index]<=min)) return [min]
     }
     return []
 };
