012c237950b0ed3a050c00aa265745630d8b8e5e
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-22 00:21:38 +0500

    Time: 78 ms (55.29%), Space: 68.2 MB (51.38%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -10,32 +10,14 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var countNodes = function (root) {
-    //Using Stacks && DFS Iterative Approach
-    // if (root == 0) {
-    //     return 0
-    // }
-    // let count = 0
-    // let stack = [root]
-    // while (stack.length > 0) {
-    //     let node = stack.pop()
-    //     if (node === null) {
-    //         continue; 
-    //     }
-    //     count++
-    //     if (node.right) {
-    //         stack.push(node.right)
-    //     }
-    //     if (node.left) {
-    //         stack.push(node.left)
-    //     }
-    // }
-    // return count
-
-    //Using DFS Recursive Approach
-    let count  = 0
-    if(root == null){
-        return 0
+var countNodes = function(root) {
+    let count = 0
+    var again = function(root){
+        if(!root) return 0
+        count++
+        again(root.left)
+        again(root.right)
     }
-    return count + 1 + countNodes(root.left) + countNodes(root.right)
+    again(root)
+    return count
 };
