4e51418e6098eecf5e78025326eb781a00b84643
Hassan Mehmood <ihassan463m@gmail.com>
2024-09-22 16:55:00 +0500

    Time: 42 ms (95.05%), Space: 49 MB (50%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -10,54 +10,14 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var preorderTraversal = function (root) {
-    // Using Recursion
-
-    // let arr = []
-    // let again = function (root) {
-    //     if (root == null) return []
-    //     arr.push(root.val)
-    //     again(root.left)
-    //     again(root.right)
-    //     return arr
-    // }
-    // return again(root)
-
-    // Using DFS
-    // let stack = [root]
-    // if (!root) return []
-    // let arr = []
-    // while (stack.length > 0) {
-    //     let length = stack.length
-    //     for (let i = 0; i < length; i++) {
-    //         let first = stack.pop()
-    //         arr.push(first.val)
-    //         if (first.right != null) {
-    //             stack.push(first.right)
-    //         }
-    //         if (first.left != null) {
-    //             stack.push(first.left)
-    //         }
-    //     }
-    // }
-    // return arr
-
-    // Using BFS
-    let queue = [root]
-    if (!root) return []
+var preorderTraversal = function(root) {
     let arr = []
-    while (queue.length > 0) {
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-            let first = queue.pop()
-            arr.push(first.val)
-            if (first.right != null) {
-                queue.push(first.right)
-            }
-            if (first.left != null) {
-                queue.push(first.left)
-            }
-        }
+    var again = function(root){
+        if(!root) return []
+        arr.push(root.val)
+        again(root.left)
+        again(root.right)
     }
+    again(root)
     return arr
 };
