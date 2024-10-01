6ba8d7d96808a978b006429f83f79c3c297fcec0
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-01 20:25:59 +0500

    Time: 77 ms (44.68%), Space: 55.5 MB (62.51%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function (numCourses, prerequisites) {
+    let graph = new Array(numCourses).fill(0).map(() => []); 
+    let indegree = new Array(numCourses).fill(0)
+    let visit = new Array(numCourses).fill(false)
+    let arr = []
+    let queue = []
+    for (let [pre, crs] of prerequisites) {
+        graph[crs].push(pre)
+        indegree[pre]++
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (indegree[i] == 0) {
+            queue.push(i)
+        }
+    }
+
+    while (queue.length > 0) {
+        let first = queue.shift()
+        arr.push(first)
+        for (let neighbor of graph[first] || []) {
+            indegree[neighbor]--
+            if (indegree[neighbor] == 0) {
+                queue.push(neighbor)
+            }
+        }
+    }
+    return arr.length == numCourses ? arr : []
+};
