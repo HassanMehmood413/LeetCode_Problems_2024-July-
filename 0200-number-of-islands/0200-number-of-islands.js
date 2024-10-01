5f91074af8e8103cf9b92a0dacf47547edaf97c4
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-01 07:31:37 +0500

    Time: 83 ms (38.71%), Space: 59.4 MB (30.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -4,41 +4,35 @@
  */
 var numIslands = function (grid) {
 
-    let bfs = function (r, c) {
-        let queue = [[r, c]];
-        grid[r][c] = "0"; // Mark as visited
-
+    var bfs = function (i, j) {
+        let queue = [[i, j]]
         while (queue.length > 0) {
-            let [row, col] = queue.shift();
-
-            // Check all four directions: up, down, left, right
-            for (let [dx, dy] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
-                let x = row + dx;
-                let y = col + dy;
-
-                // Check boundaries and if the current cell is land ('1')
-                if (x >= 0 && y >= 0 && x < rows && y < cols && grid[x][y] == "1") {
-                    grid[x][y] = "0"; // Mark as visited
-                    queue.push([x, y]); // Add to the queue to explore its neighbors
+            let length = queue.length
+            for (let i = 0; i < length; i++) {
+                let [row, col] = queue.shift()
+                for (let [r, c] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+                    let x = r + row
+                    let y = c + col
+                    if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == "1") {
+                        grid[x][y] = "0"
+                        queue.push([x, y])
+                    }
                 }
-            }
-        }
-    };
-
-    let rows = grid.length;
-    let cols = grid[0].length;
-    let number = 0;
-
-    // Traverse the grid
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (grid[r][c] == "1") {
-                number++;  // Found a new island
-                bfs(r, c); // Perform BFS to mark the entire island
             }
         }
     }
 
-    return number;
+
+
+    let number = 0
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == "1") {
+                number++
+                bfs(i, j)
+            }
+        }
+    }
+    return number
 }
 
