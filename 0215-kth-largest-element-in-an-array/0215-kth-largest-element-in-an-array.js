84a7ad6227287e2c6a20dd356cc0b4d14251c379
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-05 14:30:11 +0500

    Time: 106 ms (78.74%), Space: 69.2 MB (32.73%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -3,24 +3,72 @@ class minPriorityQueue {
         this.heap = [];
     }
 
-    enqueue(value) {
-        let left = 0;
-        let right = this.heap.length;
+    getParentIndex(i) {
+        return Math.floor((i - 1) / 2);
+    }
 
-        while (left < right) {
-            const mid = Math.floor((left + right) / 2);
-            if (this.heap[mid] < value) {
-                left = mid + 1;
+    getLeftChildIndex(i) {
+        return 2 * i + 1;
+    }
+
+    getRightChildIndex(i) {
+        return 2 * i + 2;
+    }
+
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+
+    enqueue(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = this.getParentIndex(index);
+            if (this.heap[parentIndex] > this.heap[index]) {
+                this.swap(parentIndex, index);
+                index = parentIndex;
             } else {
-                right = mid;
+                break;
             }
         }
-        this.heap.splice(left, 0, value)
     }
 
     dequeue() {
         if (this.heap.length === 0) return null;
-        return this.heap.shift();
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return min;
+    }
+
+    heapifyDown() {
+        let index = 0;
+        const length = this.heap.length;
+        while (true) {
+            const leftChildIndex = this.getLeftChildIndex(index);
+            const rightChildIndex = this.getRightChildIndex(index);
+            let smallest = index;
+
+            if (leftChildIndex < length && this.heap[leftChildIndex] < this.heap[smallest]) {
+                smallest = leftChildIndex;
+            }
+
+            if (rightChildIndex < length && this.heap[rightChildIndex] < this.heap[smallest]) {
+                smallest = rightChildIndex;
+            }
+
+            if (smallest !== index) {
+                this.swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
     }
 
     front() {
@@ -36,23 +84,32 @@ class minPriorityQueue {
     }
 }
 
-
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-    let pq = new minPriorityQueue()
-    for (let i = 0; i < k; i++) {
-        pq.enqueue(nums[i])
+    // Handle the edge case when k > nums.length
+    if (k > nums.length) {
+        return Math.min(...nums);
     }
-    for (let i = pq.size(); i < nums.length; i++) {
-        if (nums[i] >= pq.front()) {
-            let a = pq.dequeue()
-            pq.enqueue(nums[i])
+
+    let pq = new minPriorityQueue();
+    
+    // Build a min-heap of the first k elements
+    for (let i = 0; i < k; i++) {
+        pq.enqueue(nums[i]);
+    }
+    
+    // Iterate through the rest of the elements
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] > pq.front()) {
+            pq.dequeue();
+            pq.enqueue(nums[i]);
         }
     }
-    return pq.front()
-
+    
+    // The root of the heap will contain the k-th largest element
+    return pq.front();
 };
