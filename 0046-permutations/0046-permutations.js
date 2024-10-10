e10618f979a2b40adae81b5e5271aa822e7803bc
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-10 15:37:56 +0500

    Time: 79 ms (17.31%), Space: 53.5 MB (48.5%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    let res = []
+    var again = function (nums, arr, res) {
+        if (arr.length == nums.length) {
+            res.push([...arr])
+            return
+        }
+        for (let i of nums) {
+            if(arr.includes(i)){
+                continue
+            }
+            arr.push(i)
+            again(nums, arr, res)
+            arr.pop()
+        }
+    }
+    again(nums, [], res)
+    return res
+};
