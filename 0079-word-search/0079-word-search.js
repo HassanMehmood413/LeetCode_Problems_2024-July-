699eddedb239d78e507be27f9d8d697d1a8e0ab2
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-12 17:16:53 +0500

    Time: 498 ms (36.39%), Space: 54.9 MB (36.34%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    let rows = board.length
+    let cols = board[0].length
+    var dfs = function (start, end, index, board, word) {
+        if (index == word.length) return true
+        if (start < 0 || end < 0 || start >= rows || end >= cols || board[start][end] != word[index]) return false
+
+        let temp = board[start][end]
+        board[start][end] = "#"
+        for (let [x, y] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+            if (dfs(x + start, y + end, index + 1, board, word)) {
+                return true
+            }
+        }
+        board[start][end] = temp
+        return false
+    }
+
+
+
+
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (board[i][j] === word[0] && dfs(i, j, 0, board, word)) {
+                return true
+            }
+        }
+    }
+    return false
+};  
