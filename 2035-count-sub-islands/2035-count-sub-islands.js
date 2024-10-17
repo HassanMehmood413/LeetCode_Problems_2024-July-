340395741fc41b18b2ffbb0e3846fe596a93e129
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-17 17:51:38 +0500

    Time: 191 ms (27.16%), Space: 71.1 MB (39.52%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.js
+++ 2035-count-sub-islands/2035-count-sub-islands.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var countSubIslands = function (grid1, grid2) {
-    let rows = grid1.length
-    let cols = grid1[0].length
+    let row = grid1.length
+    let col = grid1[0].length
     let count = 0
     var bfs = function (start, end, grid1, grid2) {
-        let issub = true
         let queue = []
+        let issub = true
         queue.push([start, end])
         while (queue.length > 0) {
             let [first, second] = queue.shift()
@@ -17,11 +17,12 @@ var countSubIslands = function (grid1, grid2) {
                 issub = false
             }
             grid2[first][second] = 0
+
             for (let [a, b] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
                 let x = a + first
                 let y = b + second
 
-                if (x >= 0 && y >= 0 && x < rows && y < cols && grid2[x][y] == 1) {
+                if (x >= 0 && y >= 0 && x < row && y < col && grid2[x][y] == 1) {
                     queue.push([x, y])
                     grid2[x][y] = 0
                 }
@@ -29,8 +30,8 @@ var countSubIslands = function (grid1, grid2) {
         }
         return issub
     }
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
             if (grid2[i][j] == 1) {
                 if (bfs(i, j, grid1, grid2)) {
                     count++
@@ -39,5 +40,4 @@ var countSubIslands = function (grid1, grid2) {
         }
     }
     return count
-
 };
