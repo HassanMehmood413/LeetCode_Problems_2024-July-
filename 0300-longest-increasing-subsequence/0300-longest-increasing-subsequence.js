830e4415b8b2bd537cf12c8c5890b80f5d2960c9
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-22 16:58:18 +0500

    Time: 78 ms (78.45%), Space: 50.9 MB (49.56%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+    let lis = new Array(nums.length).fill(1)
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                lis[i] = Math.max(lis[i], 1 + lis[j])
+            }
+        }
+    }
+    return Math.max(...lis)
+
+};
