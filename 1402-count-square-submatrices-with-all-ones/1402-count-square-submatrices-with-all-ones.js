0bf423ef20505e31193eb836b04852c54802f4c7
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-27 08:41:38 +0500

    Time: 20 ms (60%), Space: 57.6 MB (27.27%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var countSquares = function (matrix) {
+    let dp = Array.from({ length: matrix.length }, () => Array(matrix[0].length).fill(0))
+    let max = 0
+    let sum = 0
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] == '1') {
+
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                }
+                max = Math.max(dp[i][j], max)
+            }
+        }
+    }
+    for (let i = 0; i < dp.length; i++) {
+        for (let j = 0; j < dp[0].length; j++) {
+            sum = dp[i][j] + sum
+        }
+    }
+    return sum
+};
