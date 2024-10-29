f1fef06bd6829459a3f8ca6a14eed6f062386ca6
Hassan Mehmood <ihassan463m@gmail.com>
2024-10-29 07:54:39 +0500

    Time: 20 ms (80%), Space: 62.5 MB (28.57%) - LeetHub

---
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxMoves = function (grid) {
+    let every = Array.from({ length: grid.length }, () => Array(grid[0].length).fill(-1));
+
+    var dfs = function (start, end) {
+        if (every[start][end] != -1) return every[start][end]
+        maxmoves = 0
+        for (let [x, y] of [[-1, 1], [0, 1], [1, 1]]) {
+            let a = x + start
+            let b = y + end
+            if (a >= 0 && b >= 0 && a < grid.length && b < grid[0].length && grid[a][b] > grid[start][end]) {
+                maxmoves = Math.max(maxmoves, 1 + dfs(a, b))
+            }
+        }
+        every[start][end] = maxmoves
+        return maxmoves
+    }
+
+
+
+    let maxmoves = 0
+    for (let i = 0; i < grid.length; i++) {
+        maxmoves = Math.max(maxmoves, dfs(i, 0))
+    }
+    return maxmoves
+};
