d2cb7b4c95c8aa07089f85129cefbd2dad5b8c0d
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-02 19:54:56 +0500

    Time: 10 ms (11.7%), Space: 54.2 MB (15.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,35 +3,26 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-
-
-    // check Column
-    let arr = []
-    let arr2 = []
-    for (let i = 0; i < matrix[0].length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            arr.push(matrix[j][i])
+    // check columns 
+    let columns = []
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] == 0) {
+                columns.push(j)
+            }
         }
-        if (arr.includes(0)) {
-            arr2.push(i)
-        }
-        arr = []
     }
+    console.log(columns)
     for (let i = 0; i < matrix.length; i++) {
         if (matrix[i].includes(0)) {
-            for (let j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = matrix[i][j] * 0
-            }
+            matrix[i].fill(0)
         }
     }
 
-    for (let i = 0; i < matrix[0].length; i++) {
-        let a = arr2[0]
-        if (i === a) {
-            arr2.shift()
-            for (let j = 0; j < matrix.length; j++) {
-                matrix[j][i] = matrix[j][i] * 0
-            }
+    for (let i = 0; i < columns.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            matrix[j][columns[i]] = 0
+
         }
     }
     return matrix
