d11685f7ce688b4ae49c8b082310567078f2b93e
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-04 05:06:30 +0500

    Time: 25 ms (90.7%), Space: 52.1 MB (96.69%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.js
+++ 0115-distinct-subsequences/0115-distinct-subsequences.js
@@ -4,21 +4,32 @@
  * @return {number}
  */
 var numDistinct = function (s, t) {
-    let map = {}
-    var again = function (i, j) {
-        if (j == t.length) return 1
-        if (i == s.length) return 0
-        let key = `${i},${j}`
-        if (key in map) {
-            return map[key]
+    // let map = {}
+    // var again = function (i, j) {
+    //     if (j == t.length) return 1
+    //     if (i == s.length) return 0
+    //     let key = `${i},${j}`
+    //     if (key in map) {
+    //         return map[key]
+    //     }
+    //     if (s[i] == t[j]) {
+    //         map[key] = again(i + 1, j + 1) + again(i + 1, j)
+    //     }
+    //     else {
+    //         map[key] = again(i + 1, j)
+    //     }
+    //     return map[key]
+    // }
+    // return again(0, 0)
+
+    let dp = Array(t.length + 1).fill(0)
+    dp[0] = 1
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = t.length; j >= 1; j--) {
+            if (s[i - 1] == t[j - 1]) {
+                dp[j] += dp[j - 1]
+            }
         }
-        if (s[i] == t[j]) {
-            map[key] = again(i + 1, j + 1) + again(i + 1, j)
-        }
-        else {
-            map[key] = again(i + 1, j)
-        }
-        return map[key]
     }
-    return again(0, 0)
+    return dp[t.length]
 };
