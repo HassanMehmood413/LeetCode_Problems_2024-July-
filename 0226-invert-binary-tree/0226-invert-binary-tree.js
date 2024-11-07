617247ebc88d1703c934a19296e62e86ae7c8340
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-07 16:16:58 +0500

    Time: 0 ms (100%), Space: 50.6 MB (6.45%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,14 +11,20 @@
  * @return {TreeNode}
  */
 var invertTree = function (root) {
-    if (!root) return null;
-    let queue = [root];
-    while (queue.length > 0) {
-        const node = queue.shift();
-        [node.left, node.right] = [node.right, node.left];
+    // if (!root) return null;
+    // let queue = [root];
+    // while (queue.length > 0) {
+    //     const node = queue.shift();
+    //     [node.left, node.right] = [node.right, node.left];
 
-        if (node.left !== null) queue.push(node.left);
-        if (node.right !== null) queue.push(node.right);
-    }
-    return root;
+    //     if (node.left !== null) queue.push(node.left);
+    //     if (node.right !== null) queue.push(node.right);
+    // }
+    // return root;
+
+    if (!root) return null;
+    [root.left, root.right] = [root.right, root.left]
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
 };
