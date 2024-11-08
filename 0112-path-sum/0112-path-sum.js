43c4e8747849aacdb64cf8e451f6882bdecfaf5e
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-08 06:36:15 +0500

    Time: 0 ms (100%), Space: 51.9 MB (75.92%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -12,25 +12,37 @@
  * @return {boolean}
  */
 var hasPathSum = function (root, targetSum) {
-    var again = function (root, sum, arr) {
-        if (!root) return
-        arr.push(root.val)
-        sum = sum + root.val
-        if (root.left == null && root.right == null && sum == targetSum) {
-            return true
+    // var again = function (root, sum, arr) {
+    //     if (!root) return
+    //     arr.push(root.val)
+    //     sum = sum + root.val
+    //     if (root.left == null && root.right == null && sum == targetSum) {
+    //         return true
+    //     }
+    //     else {
+    //         if (again(root.left, sum, arr) ||
+    //             again(root.right, sum, arr)) {
+    //             return true
+    //         }
+    //     }
+    //     arr.pop()
+    // }
+    // if (again(root, 0, [])) {
+    //     return true
+    // }
+    // return false
+
+
+    var again = function (root, targetSum, sum) {
+        if (!root) return false
+        sum += root.val
+        if (!root.left && !root.right) {
+            if (sum === targetSum) return true
         }
-        else {
-            if (again(root.left, sum, arr) ||
-                again(root.right, sum, arr)) {
-                return true
-            }
-        }
-        arr.pop()
+
+        return again(root.left, targetSum, sum) || again(root.right, targetSum, sum)
     }
-    if (again(root, 0, [])) {
-        return true
-    }
-    return false
+    return again(root, targetSum, 0)
 
 
 
