abc54016580ebe67534254745cdfaa98d16e61bc
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-11 01:17:27 +0500

    Time: 225 ms (72%), Space: 48.9 MB (96.33%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+var makesquare = function (matchsticks) {
+    let sum = matchsticks.reduce((a, v) => a + v, 0)
+    if (sum % 4 !== 0) {
+        return false
+    }
+    let target = sum / 4
+    matchsticks = matchsticks.sort((a, b) => b - a)
+    let array = new Array(4).fill(0)
+    var again = function (start) {
+        if (start == matchsticks.length) {
+            return true
+        }
+        for (let i = 0; i < 4; i++) {
+            if (matchsticks[start] + array[i] <= target) {
+                array[i] = matchsticks[start] + array[i]
+                if (again(start + 1)) return true
+                array[i] = array[i] - matchsticks[start] 
+            }
+        }
+        return false
+    }
+    return again(0)
+};
