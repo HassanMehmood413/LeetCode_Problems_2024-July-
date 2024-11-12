a869f1186f0579c3c88f6e9908b23cb6e129d11a
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-12 08:36:05 +0500

    Time: 2 ms (61.76%), Space: 54.3 MB (66.28%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -11,18 +11,41 @@
  * @return {string}
  */
 var tree2str = function (root) {
-    let str = ''
-    var again = function (root) {
-        if (!root) return ''
-        str = `${root.val}`
-        if (root.left || root.right) {
-            str = str + `(${again(root.left)})`
+    // let str = ''
+    // var again = function (root) {
+    //     if (!root) return ''
+    //     str = `${root.val}`
+    //     if (root.left || root.right) {
+    //         str = str + `(${again(root.left)})`
+    //     }
+    //     if (root.right) {
+    //         str = str + `(${again(root.right)})`
+    //     }
+    //     return str
+    // }
+    // let res = again(root)
+    // return res
+
+    var again = function (root, res) {
+        if (!root) return
+        res.push(root.val)
+        if (root.left) {
+            res.push('(')
+            again(root.left, res)
+            res.push(')')
+        }
+        if (!root.left && root.right) {
+            res.push("()")
         }
         if (root.right) {
-            str = str + `(${again(root.right)})`
+            res.push("(")
+            again(root.right, res)
+            res.push(')')
         }
-        return str
+        return res
     }
-    let res = again(root)
-    return res
+    let res = again(root, [])
+    return res.join("")
+
+
 };
