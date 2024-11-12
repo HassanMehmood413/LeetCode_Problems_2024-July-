c7d3a4bccd0530a43759ae1b878b2443078b4e65
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-12 16:30:40 +0500

    Time: 7 ms (86.27%), Space: 54.9 MB (52.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    s = s.split('')
-    let l = 0
-    let longest = 0
-    let map = new Map()
-    for (let i = 0; i < s.length; i++) {
-        if (map.has(s[i])) {
-            l = Math.max(l, map.get(s[i]) + 1)
+    let count = 0
+    let str = ''
+    let max = -Infinity
+    for (let right = 0; right < s.length; right++) {
+        while (str.includes(s[right])) {
+            str = str.slice(1)
         }
-
-        map.set(s[i], i)
-        longest = Math.max(longest, i - l + 1)
-
+        str = str + s[right]
+        max = Math.max(max, str.length)
     }
-    return longest
+    return max == -Infinity ? 0 : max
 };
