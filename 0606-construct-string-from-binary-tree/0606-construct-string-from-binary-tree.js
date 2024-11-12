101cf5aa5eb2b5a1e47e2678c9df1c5699358df4
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-12 08:27:48 +0500

    Time: 49 ms (8.82%), Space: 58.2 MB (6.98%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function (root) {
+    let str = ''
+    var again = function (root) {
+        if (!root) return ''
+        str = `${root.val}`
+        if (root.left || root.right) {
+            str = str + `(${again(root.left)})`
+            console.log(str)
+        }
+        if (root.right) {
+            str = str + `(${again(root.right)})`
+        }
+        return str
+    }
+    let res = again(root)
+    return res
+};
