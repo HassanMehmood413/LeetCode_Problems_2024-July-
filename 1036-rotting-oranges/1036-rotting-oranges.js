506581f10b1096c7c6d6b6220a6bb52ce719d964
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-13 13:33:05 +0500

    Time: 12 ms (48.28%), Space: 53.8 MB (41.28%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.js
+++ 1036-rotting-oranges/1036-rotting-oranges.js
@@ -3,41 +3,37 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    let queue = []
     let fresh = 0
     let min = 0
-    let x = 0
-    let y = 0
-    let queue = []
-
-    for (let r = 0; r < grid.length; r++) {
-        for (let c = 0; c < grid[0].length; c++) {
-            if (grid[r][c] == "2") {
-                queue.push([r, c])
-            }
-            else if (grid[r][c] == "1") {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == 1) {
                 fresh++
+            }
+            else if (grid[i][j] == 2) {
+                queue.push([i, j])
             }
         }
     }
-
-
     while (queue.length > 0 && fresh > 0) {
         let length = queue.length
         for (let i = 0; i < length; i++) {
-            let [rows, cols] = queue.shift()
+            let [first, second] = queue.shift()
+            for (let [x, y] of [[-1, 0], [0, -1], [1, 0], [0, 1]]) {
+                let a = x + first
+                let b = y + second
 
-            for (let [row, col] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
-                x = row + rows
-                y = col + cols
-                if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == "1") {
-                    grid[x][y] = "2"
+                if (a >= 0 && b >= 0 && a < grid.length && b < grid[0].length && grid[a][b] == 1) {
+                    queue.push([a, b])
+                    grid[a][b] = 2
                     fresh--
-                    queue.push([x, y])
                 }
             }
         }
         min++
     }
 
-    return fresh === 0 ? min : -1
+
+    return fresh == 0 ? min : -1
 };
