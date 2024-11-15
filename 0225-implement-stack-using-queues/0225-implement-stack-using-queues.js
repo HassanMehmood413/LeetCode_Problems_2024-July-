9d1c0830c6bdfdf809780664ad5067479400e94f
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-15 18:14:06 +0500

    Time: 0 ms (100%), Space: 48.7 MB (60.78%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -1,44 +1,41 @@
 
-var MyStack = function() {
+var MyStack = function () {
     this.q1 = []
     this.q2 = []
-
 };
-// q1 = [5,2,3,4]
-// q2 = []
 
 /** 
  * @param {number} x
  * @return {void}
  */
-MyStack.prototype.push = function(x) {
-  while(this.q1.length !=0){
-    this.q2.push(this.q1.shift())
-  }
+MyStack.prototype.push = function (x) {
+    while(this.q1.length != 0){
+        this.q2.push(this.q1.shift())
+    }
     this.q1.push(x)
-  while(this.q2.length !=0){
-    this.q1.push(this.q2.shift())
-  }
+    while(this.q2.length != 0){
+        this.q1.push(this.q2.shift())
+    }
 };
 
 /**
  * @return {number}
  */
-MyStack.prototype.pop = function() {
+MyStack.prototype.pop = function () {
     return this.q1.shift()
 };
 
 /**
  * @return {number}
  */
-MyStack.prototype.top = function() {
+MyStack.prototype.top = function () {
     return this.q1[0]
 };
 
 /**
  * @return {boolean}
  */
-MyStack.prototype.empty = function() {
+MyStack.prototype.empty = function () {
     return this.q1.length == 0
 };
 
