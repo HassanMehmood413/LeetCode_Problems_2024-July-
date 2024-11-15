ab48b7f98a9855f7b643aa551655082842252e79
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-15 18:17:37 +0500

    Time: 18 ms (94.12%), Space: 63.5 MB (53.46%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
@@ -3,15 +3,14 @@
  * @return {string}
  */
 var removeStars = function (s) {
-    s = s.split('')
-    stack = []
-    for (let i = 0; i <= s.length - 1; i++) {
-        if (s[i] != '*') {
-            stack.push(s[i])
-        }
-        else if (s[i] == '*') {
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "*") {
             stack.pop()
         }
+        else {
+            stack.push(s[i])
+        }
     }
-    return stack.join('')
+    return stack.join("")
 };
