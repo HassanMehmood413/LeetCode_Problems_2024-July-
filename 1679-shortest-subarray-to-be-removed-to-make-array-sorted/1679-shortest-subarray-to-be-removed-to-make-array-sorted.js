9e1bf1561fb480a2cca711f7b12dabc3f351e239
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-15 06:42:39 +0500

    Time: 0 ms (100%), Space: 60.9 MB (42.86%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.js
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var findLengthOfShortestSubarray = function (arr) {
-    let min = Infinity
     let left = 0
     while (left < arr.length - 1 && arr[left] <= arr[left + 1]) {
         left++
@@ -13,16 +12,17 @@ var findLengthOfShortestSubarray = function (arr) {
     while (right > 0 && arr[right - 1] <= arr[right]) {
         right--
     }
-    let minLength = Math.min(arr.length - left - 1, right);
+
+    let min = Math.min(arr.length - left - 1, right)
     let i = 0, j = right
     while (i <= left && j < arr.length) {
         if (arr[i] <= arr[j]) {
-            minLength = Math.min(minLength, j - i - 1)
+            min = Math.min(min, j - i - 1)
             i++
         }
         else {
             j++
         }
     }
-    return minLength
+    return min
 };
