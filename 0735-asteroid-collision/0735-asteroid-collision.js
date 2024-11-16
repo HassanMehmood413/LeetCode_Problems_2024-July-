1e2f108b7cc9b4c3dbb948f3da27df4c057e0c4f
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-16 19:59:17 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -5,42 +5,38 @@
 var asteroidCollision = function (asteroids) {
     let stack = []
     for (let i = 0; i < asteroids.length; i++) {
-        if (stack.length == 0) {
+        // for same
+        if (stack[stack.length - 1] == asteroids[i]) {
+            stack.pop()
+        }
+        else if (stack[stack.length - 1] > 0 && asteroids[i] > 0) {
             stack.push(asteroids[i])
         }
-        else {
-            if (stack[stack.length - 1] < 0 && asteroids[i] > 0) {
-                stack.push(asteroids[i])
-            }
-            else if (stack[stack.length - 1] > 0 && asteroids[i] < 0) {
-
-                while (stack.length > 0 && stack[stack.length - 1] > 0 && asteroids[i] < 0) {
-                    a = asteroids[i] * -1
-                    if (a > stack[stack.length - 1]) {
-                        stack.pop()
-                        if (stack.length === 0 || stack[stack.length - 1] < 0) {
-                            stack.push(asteroids[i]);
-                            break;
-                        }
-                    }
-                    else if (a == stack[stack.length - 1]) {
-                        stack.pop()
-                        break
-                    }
-                    else {
+        else if (stack[stack.length - 1] < 0 && asteroids[i] > 0) {
+            stack.push(asteroids[i])
+        }
+        else if (stack[stack.length - 1] > 0 && asteroids[i] < 0) {
+            while (stack.length > 0 && stack[stack.length - 1] > 0 && asteroids[i] < 0) {
+                a = asteroids[i] * -1
+                if (a > stack[stack.length - 1]) {
+                    stack.pop()
+                    if (stack.length == 0 || stack[stack.length - 1] < 0) {
+                        stack.push(asteroids[i])
                         break
                     }
                 }
+                else if (a == stack[stack.length - 1]) {
+                    stack.pop()
+                    break
+                }
+                else {
+                    break
+                }
             }
-            else if (stack[stack.length - 1] > 0 && asteroids[i] > 0) {
-                stack.push(asteroids[i])
-            }
-            else {
-                stack.push(asteroids[i])
-            }
-
+        }
+        else {
+            stack.push(asteroids[i])
         }
     }
-
     return stack
 };
