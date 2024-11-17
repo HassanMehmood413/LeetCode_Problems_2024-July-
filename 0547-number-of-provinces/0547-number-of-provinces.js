0f260773478add702dd016a8a140b000a6f86a50
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-17 07:56:46 +0500

    Time: 3 ms (60.04%), Space: 51.1 MB (79.92%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -5,22 +5,25 @@
 var findCircleNum = function (isConnected) {
     var dfs = function (start, visit) {
         visit[start] = true
-        let neighborindex = 0
+        let index = 0
         for (let neighbor of isConnected[start]) {
-            if (neighbor == 1 && !visit[neighborindex]) {
-                dfs(neighborindex, visit)
+            if (neighbor == 1 && !visit[index]) {
+                dfs(index, visit)
             }
-            neighborindex++
+            index++
         }
     }
 
-    let provience = 0
+
+    let count = 0
     let visit = new Array(isConnected.length).fill(false)
     for (let i = 0; i < isConnected.length; i++) {
         if (!visit[i]) {
-            provience++
+            count++
             dfs(i, visit)
         }
     }
-    return provience
+    return count
+
+
 };
