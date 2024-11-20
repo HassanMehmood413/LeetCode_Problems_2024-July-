666e17bef5c12525a7f3a0f7266a58092bfa6581
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-20 21:01:07 +0500

    Time: 1 ms (85.72%), Space: 58.8 MB (20.6%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let max = nums[0]
+    let max = -Infinity
     let sum = 0
-    for (let i = 0; i < nums.length; i++) {
-        sum = sum + nums[i]
-        if (sum > max) {
-            max = sum
-
-        }
+    for (let right = 0; right < nums.length; right++) {
+        sum = sum + nums[right]
         if (sum < 0) {
             sum = 0
         }
+        else {
+            max = Math.max(sum, max)
+        }
+
     }
-    return max
+    return max == -Infinity ? Math.max(...nums) : max
 };
