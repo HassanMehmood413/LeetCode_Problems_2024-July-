5a3e4c134b7abcbc2a6ee887e5bd697d5ffaa810
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-22 12:22:22 +0500

    Time: 0 ms (100%), Space: 49.2 MB (34.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,61 +4,27 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-    let rows = matrix.length
-    let cols = matrix[0].length
-    let left = 0
-    let right = rows * cols-1
-    while (left <= right) {
-        let middle = Math.floor((left + right) / 2)
-        let row = Math.floor(middle / cols)
-        let col = Math.floor(middle % cols)
-        let a = matrix[row][col]
-        if (a == target) return true
-        else if (a > target) right = middle - 1
-        else left = middle + 1
+    let isvalid = false
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] <= target && matrix[i][matrix[i].length - 1] >= target) {
+            let left = 0
+            let right = matrix[i].length
+            while (left <= right) {
+                let mid = Math.floor((left + right) / 2)
+                if (matrix[i][mid] == target) {
+                    return isvalid = true
+                }
+                else if (matrix[i][mid] > target) {
+                    right = mid - 1
+                }
+                else {
+                    left = mid + 1
+                }
+            }
+        }
+        if(isvalid == true){
+            return true
+        }
     }
-    return false
-
-
-
-
-
-    // let arr = matrix.flat()
-    // let left = 0
-    // let right = arr.length - 1
-    // while (left <= right) {
-    //     let mid = Math.floor((left + right) / 2)
-    //     if (arr[mid] == target) {
-    //         return true
-    //     }
-    //     else if (arr[mid] > target) {
-    //         right = mid - 1
-    //     }
-    //     else {
-    //         left = mid + 1
-    //     }
-    // }
-    // return false
-
-
-    // Both have 0(m*n) Complexity:
-    // let row = 0
-    // let col = matrix[0].length - 1
-    // while (row <= matrix.length - 1 && col >= 0) {
-    //     let el = matrix[row][col]
-    //     if (el == target) {
-    //         return true
-    //     }
-    //     else if (el > target) {
-    //         col--
-    //     }
-    //     else if (el < target) {
-    //         row++
-    //     }
-    // }
-    // return false
-
-
-
-
-}
+    return isvalid
+};
