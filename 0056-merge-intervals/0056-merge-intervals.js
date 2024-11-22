2d5902080ea31f813191bea608c8e972e13e27b5
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-22 05:43:50 +0500

    Time: 14 ms (28.87%), Space: 59 MB (45.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,21 +3,24 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-    intervals = intervals.sort((a, b) => a[0] - b[0]);
+
     let arr = []
-    for (let i = 0; i <= intervals.length - 1; i++) {
+    intervals = intervals.sort((a, b) => a[0] - b[0])
+    for (let i = 0; i < intervals.length; i++) {
         if (arr.length == 0) {
             arr.push(intervals[i])
         }
         else if (intervals[i][0] <= arr[arr.length - 1][1]) {
-            a = Math.min(intervals[i][1], arr[arr.length - 1][0])
-            b = Math.max(intervals[i][1], arr[arr.length - 1][1])
+            let first = intervals[i][0]
+            let second = arr[arr.length - 1][0]
+            let third = intervals[i][1]
+            let fourth = arr[arr.length - 1][1]
             arr.pop()
-            arr.push([a, b])
+            arr.push([Math.min(first, second), Math.max(third, fourth)])
         }
         else {
             arr.push(intervals[i])
         }
     }
     return arr
-};
+}
