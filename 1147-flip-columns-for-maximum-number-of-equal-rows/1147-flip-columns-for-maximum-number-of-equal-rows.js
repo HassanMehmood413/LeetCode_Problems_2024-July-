8f07fe54d605f0c9173756b9e84ab0e2f760e8fd
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-22 07:32:07 +0500

    Time: 91 ms (20%), Space: 60.2 MB (8.7%) - LeetHub

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxEqualRowsAfterFlips = function (matrix) {
+    let map = new Map()
+    for (let i = 0; i < matrix.length; i++) {
+        let newrow = matrix[i].map((item, index) => item == matrix[i][0] ? 0 : 1).join(',')
+        if (map.has(newrow)) {
+            map.set(newrow, map.get(newrow) + 1)
+        }
+        else {
+            map.set(newrow, 1)
+        }
+    }
+    let count = 0
+    for (let [value, index] of map) {
+        console.log(value,index)
+        if (index > count) {
+            count = Math.max(count, index)
+        }
+    }
+    return count
+};
