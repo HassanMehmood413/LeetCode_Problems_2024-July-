f92927417c20a0a1c0a02559ffbb97a451cb83d1
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-23 12:13:33 +0500

    Time: 403 ms (11.11%), Space: 79.4 MB (5.88%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.js
+++ 1972-rotating-the-box/1972-rotating-the-box.js
@@ -3,14 +3,27 @@
  * @return {character[][]}
  */
 var rotateTheBox = function (box) {
+    // for (let i = 0; i < box.length; i++) {
+    //     let l = 1
+    //     while (l <= box[0].length) {
+    //         while ((box[i][l] == '.' && box[i][l - 1] == '#')) {
+    //             [box[i][l], box[i][l - 1]] = [box[i][l - 1], box[i][l]]
+    //             l--
+    //         }
+    //         l++
+    //     }
+    // }
     for (let i = 0; i < box.length; i++) {
-        let l = 1
-        while (l <= box[0].length) {
-            while ((box[i][l] == '.' && box[i][l - 1] == '#')) {
-                [box[i][l], box[i][l - 1]] = [box[i][l - 1], box[i][l]]
-                l--
+        let l = 1;
+        while (l < box[i].length) {
+            // If the current cell is '.' and the previous cell is '#', swap them
+            if (box[i][l] === '.' && box[i][l - 1] === '#') {
+                [box[i][l], box[i][l - 1]] = [box[i][l - 1], box[i][l]];
+                // Move back to check if further swaps are needed
+                l = Math.max(1, l - 1); // Ensure `l` doesn't go out of bounds
+            } else {
+                l++; // Move to the next cell
             }
-            l++
         }
     }
     let res = []
