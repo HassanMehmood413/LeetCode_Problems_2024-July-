74431f675e8bbed4afeb5bc3a153300f474ca8aa
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-23 12:24:03 +0500

    Time: 361 ms (22.22%), Space: 80.8 MB (5.88%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.js
+++ 1972-rotating-the-box/1972-rotating-the-box.js
@@ -16,24 +16,20 @@ var rotateTheBox = function (box) {
     for (let i = 0; i < box.length; i++) {
         let l = 1;
         while (l < box[i].length) {
-            // If the current cell is '.' and the previous cell is '#', swap them
             if (box[i][l] === '.' && box[i][l - 1] === '#') {
                 [box[i][l], box[i][l - 1]] = [box[i][l - 1], box[i][l]];
-                // Move back to check if further swaps are needed
-                l = Math.max(1, l - 1); // Ensure `l` doesn't go out of bounds
+                l = Math.max(1, l - 1);
             } else {
-                l++; // Move to the next cell
+                l++;
             }
         }
     }
-    let res = []
-    for (let i = 0; i < box[0].length; i++) {
-        let arr = []
-        for (let j = 0; j < box.length; j++) {
-            arr.push(box[j][i])
+    let res = Array.from({ length: box[0].length }, () => []); // Pre-allocate result array
+    for (let i = 0; i < box.length; i++) {
+        for (let j = 0; j < box[i].length; j++) {
+            res[j][box.length - i - 1] = box[i][j]; // Rotate 90° clockwise
         }
-        arr.reverse()
-        res.push(arr)
+
     }
     return res
 };
