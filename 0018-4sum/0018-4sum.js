8f9cbf19b67ccd79b56576e99baeb111e1abe5e3
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-24 04:43:13 +0500

    Time: 20 ms (71.85%), Space: 53.1 MB (76.86%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -4,43 +4,40 @@
  * @return {number[][]}
  */
 var fourSum = function (nums, target) {
-    let arr = []
-    if (nums == null || nums.length < 4) {
-        return arr;
-    }
     nums = nums.sort((a, b) => a - b)
-    let k = nums.length
-    for (let i = 0; i < k - 3; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {
+    let temp = []
+    let sum = 0
+    for (let a = 0; a < nums.length - 3; a++) {
+        if (a > 0 && nums[a] == nums[a - 1]) {
             continue
         }
-        for (let j = i + 1; j < k - 2; j++) {
-            if (j > i + 1 && nums[j] == nums[j - 1]) {
+        for (let b = a + 1; b < nums.length - 2; b++) {
+            if (b > a + 1 && nums[b] == nums[b - 1]) {
                 continue
             }
-            let l = j + 1
-            let k = nums.length - 1
-            while (l < k) {
-                sum = nums[i] + nums[j] + nums[l] + nums[k]
+            let c = b + 1
+            let d = nums.length - 1
+            while (c < d) {
+                sum = nums[a] + nums[b] + nums[c] + nums[d]
                 if (sum < target) {
-                    l++
+                    c++
                 }
                 else if (sum > target) {
-                    k--
+                    d--
                 }
                 else {
-                    arr.push([nums[i], nums[j], nums[l], nums[k]])
-                    while (nums[l] === nums[l + 1] && l < k) {
-                        l++
+                    temp.push([nums[a], nums[b], nums[c], nums[d]])
+                    while (c < d && nums[c] == nums[c + 1]) {
+                        c++
                     }
-                    while (nums[k] === nums[k - 1] && l < k) {
-                        k--
+                    while (c < d && nums[d] == nums[d - 1]) {
+                        d--
                     }
-                    l++
-                    k--
+                    c++
+                    d--
                 }
             }
         }
     }
-    return arr
-};
+    return temp
+}
