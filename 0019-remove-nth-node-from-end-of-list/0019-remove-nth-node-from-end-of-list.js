cdfe9c1d758c192ac11cd3ca742c4454f64a9377
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-25 04:33:46 +0500

    Time: 0 ms (100%), Space: 51.4 MB (14.41%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,54 +11,18 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function (head, n) {
-    // let curr = head
-    // let prev = null
-    // while (curr != null) {
-    //     let temp = curr.next
-    //     curr.next = prev
-    //     prev = curr
-    //     curr = temp
-    // }
-
-
-    // let curr1 = prev
-    // let dummy = new ListNode()
-    // let prev1 = dummy
-    // let length = 0
-    // while (curr1 != null) {
-    //     if (length != n - 1) {
-    //         prev1.next = new ListNode(curr1.val)
-    //         prev1 = prev1.next
-    //     }
-    //     length++
-    //     curr1 = curr1.next
-    // }
-
-    // let curr2 = dummy.next
-    // let prev2 = null
-    // while (curr2 != null) {
-    //     let temp = curr2.next
-    //     curr2.next = prev2
-    //     prev2 = curr2
-    //     curr2 = temp
-    // }
-    // return prev2
-
-
     let slow = head
     let fast = head
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         fast = fast.next
     }
-    if(!fast){
+    if (fast == null) {
         return head.next
     }
-    while(fast.next != null){
+    while (fast.next != null) {
         slow = slow.next
         fast = fast.next
     }
     slow.next = slow.next.next
     return head
-
-    
 };
