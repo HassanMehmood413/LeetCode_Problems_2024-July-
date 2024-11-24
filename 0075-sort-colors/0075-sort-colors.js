4bd00f6c4d58fc84171698d179e6f6f54770f61b
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-24 21:55:04 +0500

    Time: 3 ms (13.51%), Space: 51.3 MB (5.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,19 +3,55 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    let left = 0, mid = 0, right = nums.length - 1
-    while (mid <= right) {
-        if (nums[mid] == 0) {
-            [nums[left], nums[mid]] = [nums[mid], nums[left]]
+    // let left = 0, mid = 0, right = nums.length - 1
+    // while (mid <= right) {
+    //     if (nums[mid] == 0) {
+    //         [nums[left], nums[mid]] = [nums[mid], nums[left]]
+    //         left++
+    //         mid++
+    //     }
+    //     else if (nums[mid] == 1) {
+    //         mid++
+    //     }
+    //     else {
+    //         [nums[mid], nums[right]] = [nums[right], nums[mid]]
+    //         right--
+    //     }
+    // } return nums
+
+    comp(nums, 0, nums.length - 1)
+    return nums
+};
+function comp(nums, start, end) {
+    if (start >= end) return 0
+    let mid = Math.floor((start + end) / 2)
+    comp(nums, start, mid)
+    comp(nums, mid + 1, end)
+    merge(nums, start, mid, end)
+}
+function merge(nums, start, mid, end) {
+    let left = start
+    let right = mid + 1
+    let temp = []
+    while (left <= mid && right <= end) {
+        if (nums[left] <= nums[right]) {
+            temp.push(nums[left])
             left++
-            mid++
-        }
-        else if (nums[mid] == 1) {
-            mid++
         }
         else {
-            [nums[mid], nums[right]] = [nums[right], nums[mid]]
-            right--
+            temp.push(nums[right])
+            right++
         }
-    } return nums
-};
+    }
+    while (left <= mid) {
+        temp.push(nums[left])
+        left++
+    }
+    while (right <= end) {
+        temp.push(nums[right])
+        right++
+    }
+    for (let i = start; i <= end; i++) {
+        nums[i] = temp[i - start]
+    }
+}
