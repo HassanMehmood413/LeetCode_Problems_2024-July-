828c9d7dd57ea4bf9b3ec1a659bbd866538fb64c
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-26 07:26:49 +0500

    Time: 46 ms (16.67%), Space: 60 MB (7.69%) - LeetHub

---
+++ 3189-find-champion-ii/3189-find-champion-ii.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findChampion = function (n, edges) {
+    let indegree = new Array(n).fill(0)
+    let graph = []
+
+    var again = function (graph, node1, node2) {
+        if (!graph[node1]) {
+            graph[node1] = []
+        }
+        graph[node1].push(node2)
+        indegree[node2]++
+    }
+
+    for (let i = 0; i < edges.length; i++) {
+        again(graph, edges[i][0], edges[i][1])
+    }
+
+    let queue = []
+    let ans = []
+    for (let i = 0; i < indegree.length; i++) {
+        if (indegree[i] == 0) {
+            queue.push(i)
+            ans.push(i)
+        }
+    }
+    console.log(queue,ans)
+
+
+
+    let res = []
+    while (queue.length > 0) {
+        let first = queue.shift()
+        res.push(first)
+        for (let neighbor of graph[first] || []) {
+            indegree[neighbor]--
+            if (indegree[neighbor] == 0) {
+                queue.push(neighbor)
+            }
+        }
+
+    }
+    console.log(res,ans)
+    if (ans.length > 1) {
+        return -1
+    }
+    else {
+        return res[0]
+    }
+
+};
