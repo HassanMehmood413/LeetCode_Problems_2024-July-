32f5c3cc41e5c93e95b25b175da5f3f9f2dde124
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-26 16:05:17 +0500

    Time: 30 ms (94.64%), Space: 66.4 MB (25.95%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,62 +3,36 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    // nums = nums.sort((a, b) => (a - b))
-
-    // let arr = []
-    // for (let i = 0; i < nums.length; i++) {
-    //     if (i > 0 && nums[i] === nums[i - 1]) {
-    //         continue;
-    //     }
-    //     j = i + 1
-    //     k = nums.length - 1
-    //     while (j < k) {
-    //         total = nums[i] + nums[j] + nums[k]
-    //         if (total > 0) {
-    //             k--
-    //         }
-    //         else if (total < 0) {
-    //             j++
-    //         }
-    //         else {
-    //             arr.push([nums[i], nums[j], nums[k]])
-    //             j++
-    //             while (nums[j] === nums[j - 1] && j < k) {
-    //                 j++
-    //             }
-    //         }
-    //     }
-    // }
-    // return arr
-
-
-    // PT:
-    nums.sort((a, b) => a - b)
-    let sum = 0
+    let res = []
     let arr = []
-    for (let a = 0; a < nums.length; a++) {
-        if (a > 0 && nums[a] == nums[a - 1]) {
+    nums = nums.sort((a, b) => a - b)
+    // [-4, -1, -1, 0, 1, 2]
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1]) {
             continue
         }
-        let b = a + 1
+        let b = i + 1
         let c = nums.length - 1
         while (b < c) {
-            sum = nums[a] + nums[b] + nums[c]
-            if (sum < 0) {
-                b++
-            }
-            else if (sum > 0) {
+            let sum = nums[i] + nums[b] + nums[c]
+            if (sum > 0) {
                 c--
             }
-            else {
-                arr.push([nums[a], nums[b], nums[c]])
+            else if (sum < 0) {
                 b++
+            }
+            else {
+                res.push([nums[i], nums[b], nums[c]])
+                b++
+                c--
                 while (b < c && nums[b] == nums[b - 1]) {
                     b++
+                }
+                while (b < c && nums[c] == nums[c + 1]) {
+                    c--
                 }
             }
         }
     }
-    return arr
-
+    return res
 };
