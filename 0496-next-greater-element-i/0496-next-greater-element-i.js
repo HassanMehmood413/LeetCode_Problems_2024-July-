a5807ee6091db1293d575c3d12ad09fb0faeb920
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-27 16:57:41 +0500

    Time: 6 ms (36.01%), Space: 56.2 MB (5.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,47 +4,23 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-    // complexity : 0(n*m2)
-
-    // let indices = []
-    // for (let i = 0; i < nums1.length; i++) {
-    //     let index = nums2.indexOf(nums1[i])
-    //     indices.push(index)
-    // }
-    // let b;
-    // let max = -1
-    // for (let i = 0; i < indices.length; i++) {
-    //     let a = indices[i]
-    //     let comp = nums2[a]
-    //     for (let j = a; j < nums2.length; j++) {
-    //         b = nums2[j]
-    //         if (b > comp) {
-    //             max = b
-    //             break
-    //         }
-    //         else {
-    //             max = -1
-    //         }
-    //     }
-    //     if (max < comp) {
-    //         nums1[i] = -1
-    //     }
-    //     else {
-    //         nums1[i] = max
-    //     }
-    // }
-    // return nums1
-
-    // Efficient Complexity: 0(n * m):
-    let map = new Map()
-    let stack = []
-    for (let num of nums2) {
-        while (stack.length > 0 && stack[stack.length - 1]<num){
-            map.set(stack.pop(),num)
-        }
-        stack.push(num)
+    for (let i = 0; i < nums1.length; i++) {
+        let a = nums1[i]
+        let res = find(nums2, a)
+        nums1[i] = res
     }
-    return nums1.map((num)=>map.get(num)||-1)
-
-
+    return nums1
 };
+var find = function (nums2, a) {
+    index = nums2.indexOf(a)
+    if (index == nums2.length - 1) return -1
+    let arr = nums2.slice(index, nums2.length)
+    let i = 0
+    while (i < arr.length) {
+        if (arr[i] > nums2[index]) {
+            return arr[i]
+        }
+        i++
+    }
+    return -1
+}
