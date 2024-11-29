c70f642df291977a3ddaab075ea3cc451383c9ed
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-29 05:01:38 +0500

    Time: 583 ms (8.72%), Space: 80 MB (81.75%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -4,34 +4,19 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function (nums, k) {
-    // let res = []
-    // let left = 0
-    // let i = k
-    // while (left <= nums.length - i) {
-
-    //     a = nums.slice(left, k)
-    //     let max = Math.max(...a)
-    //     res.push(max)
-    //     left++
-    //     k++
-
-    // }
-
-    // return res
-
-    let result = []
+    let res = []
     let deque = []
     for (let i = 0; i < nums.length; i++) {
         if (deque.length > 0 && deque[0] <= i - k) {
             deque.shift()
         }
-        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]){
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
             deque.pop()
         }
         deque.push(i)
-        if(i>=k-1){
-            result.push(nums[deque[0]])
+        if (i >= k - 1) {
+            res.push(nums[deque[0]])
         }
     }
-    return result
+    return res
 };
