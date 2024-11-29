27b5e800052b82d1b0c95b71888960112459896b
Hassan Mehmood <ihassan463m@gmail.com>
2024-11-29 06:20:23 +0500

    Time: 12 ms (49.46%), Space: 53.8 MB (44.33%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.js
+++ 1036-rotting-oranges/1036-rotting-oranges.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
-    let fresh = 0
     let queue = []
-    let min = 0
+    let fresh = 0
+    let count = 0
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] == 1) {
-                fresh++
-            }
-            else if (grid[i][j] == 2) {
+            if (grid[i][j] == 2) {
                 queue.push([i, j])
+            }
+            else if (grid[i][j] == 1) {
+                fresh++
             }
         }
     }
@@ -20,18 +20,20 @@ var orangesRotting = function (grid) {
         let length = queue.length
         for (let i = 0; i < length; i++) {
             let [first, second] = queue.shift()
-            for (let [x, y] of [[-1, 0], [0, -1], [0, 1], [1, 0]]) {
+            for (let [x, y] of [[-1, 0], [0, -1], [1, 0], [0, 1]]) {
                 let a = x + first
                 let b = y + second
 
                 if (a >= 0 && b >= 0 && a < grid.length && b < grid[0].length && grid[a][b] == 1) {
-                    fresh--
+                    grid[a][b] = 0
                     queue.push([a, b])
-                    grid[a][b] = 2
+                    fresh--
                 }
             }
         }
-        min++
+        count++
     }
-    return fresh == 0 ? min : -1
+
+
+    return fresh == 0 ? count : -1
 };
