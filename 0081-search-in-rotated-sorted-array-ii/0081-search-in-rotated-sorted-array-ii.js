289005848cc23593d0433b6060bdc9b6ef1a3ca0
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-03 15:59:06 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -4,49 +4,36 @@
  * @return {boolean}
  */
 var search = function (nums, target) {
-    // Main Problem is to avoid duplicates
-    let i = 0
-    let j = nums.length - 1
-    while (i <= j) {
-        mid = Math.floor((i + j) / 2)
-        if (nums[mid] === target) {
-            return true;
-        }
-
-        // Avoid Duplicates
-        if (nums[i] === nums[mid] && nums[j] === nums[mid]) {
-            i++;
-            j--;
-        }
-        // left half is sorted
-        else if (nums[i] <= nums[mid]) {
-            if (target == nums[mid]) {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (nums[0] <= nums[mid]) {
+            if (nums[mid] == target) {
                 return true
             }
-            else if (nums[i] <= target && target <= nums[mid]) {
-                j = mid - 1
+            if (nums[left] === nums[mid] && nums[right] === nums[mid]) {
+                left++;
+                right--;
+            }
+            else if (nums[0] <= target && target <= nums[mid]) {
+                left = mid + 1
             }
             else {
-                i = mid + 1
+                right = mid - 1
             }
-
         }
-
-        //Right half is sorted
         else {
-
-            if (target == nums[mid]) {
+            if (nums[mid] == target) {
                 return true
             }
-            else if (nums[mid] < target && target <= nums[j]) {
-                i = mid + 1
+            else if (nums[mid] >= target && target <= nums[right]) {
+                right = mid - 1
             }
             else {
-                j = mid - 1
+                left = mid + 1
             }
         }
-
     }
     return false
-
 };
