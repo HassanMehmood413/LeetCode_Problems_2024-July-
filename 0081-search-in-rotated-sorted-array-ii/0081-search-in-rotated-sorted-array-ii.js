091857d7629b621b3ed4136698b8585531510762
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-03 16:00:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -21,21 +21,21 @@ var search = function (nums, target) {
             }
 
             else if (nums[0] <= target && target <= nums[mid]) {
-                left = mid + 1
+                right = mid - 1
             }
             else {
-                right = mid - 1
+                left = mid + 1
             }
         }
         else {
             if (nums[mid] == target) {
                 return true
             }
-            else if (nums[mid] >= target && target <= nums[right]) {
-                right = mid - 1
+            else if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1
             }
             else {
-                left = mid + 1
+                right = mid - 1
             }
         }
     }
