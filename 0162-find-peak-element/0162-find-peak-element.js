66a5070a7fdee1919a6dce012d2e057e2288eca6
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-03 17:01:49 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-    let l = 0
-    let r = nums.length - 1
-
-    while (l <= r) {
-        mid = Math.floor((l + r) / 2)
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
         if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
             return mid
         }
-        else if (nums[mid + 1] > nums[mid]) {
-            l = mid + 1
+        else if (nums[mid+1] > nums[mid]) {
+            left = mid + 1
         }
         else {
-            r = mid - 1
+            right = mid - 1
         }
     }
-    return mid
+    return 0
 };
