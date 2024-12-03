9cae1115fa91d546c7113ee793152cc32b0eff68
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-03 16:10:14 +0500

    Time: 0 ms (100%), Space: 51 MB (16.94%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -8,32 +8,22 @@ var search = function (nums, target) {
     let right = nums.length - 1
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (nums[mid] == target) {
-            return true
-        }
-        if (nums[left] === nums[mid] && nums[right] === nums[mid]) {
-            left++;
-            right--;
+        if (nums[mid] == target) return true
+
+        if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            left++
+            right--
         }
         else if (nums[left] <= nums[mid]) {
-            if (nums[mid] == target) {
-                return true
-            }
-
-            else if (nums[left] <= target && target <= nums[mid]) {
-                right = mid - 1
-            }
+            if (nums[mid] == target) return true
+            else if (nums[left] <= target && target <= nums[mid]) right = mid - 1
             else {
                 left = mid + 1
             }
         }
         else {
-            if (nums[mid] == target) {
-                return true
-            }
-            else if (nums[mid] <= target && target <= nums[right]) {
-                left = mid + 1
-            }
+            if (nums[mid] == target) return true
+            else if (nums[mid] <= target && target <= nums[right]) left = mid + 1
             else {
                 right = mid - 1
             }
