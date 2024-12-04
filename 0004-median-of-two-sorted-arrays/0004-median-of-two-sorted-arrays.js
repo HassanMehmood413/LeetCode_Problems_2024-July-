d2bfb0b81ffe94c6c8e28e9aa0395f0481bdb294
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-04 12:11:40 +0500

    Time: 6 ms (60.27%), Space: 54.2 MB (83.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,47 +4,39 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-    //Step1: Check that nums1.length<nums2.length or not .if not then do swap the arrays
     if (nums1.length > nums2.length) {
-        [nums1, nums2] = [nums2, nums1];
+        [nums1, nums2] = [nums2, nums1]
     }
+    let m = nums1.length
+    let n = nums2.length
 
-    let m = nums1.length;
-    let n = nums2.length;
+    let low = 0
+    let high = m
+    while (low <= high) {
+        let mid1 = Math.floor((low + high) / 2)
+        let mid2 = Math.floor((n + m + 1) / 2) - mid1
 
-    let start = 0;
-    let end = m;
+        let l1 = mid1 == 0 ? -Infinity : nums1[mid1 - 1]
+        let r1 = mid1 == m ? Infinity : nums1[mid1]
+        let l2 = mid2 == 0 ? -Infinity : nums2[mid2 - 1]
+        let r2 = mid2 == n ? Infinity : nums2[mid2]
 
-    while (start <= end) {
 
-        //Step2: Check the partitions 
-        const partition1 = Math.floor((start + end) / 2);
-        const partition2 = Math.floor((n + m + 1) / 2) - partition1;
-
-        //Step3: Determine the min and max value 
-        let maxLeft1 = partition1 === 0 ? -Infinity : nums1[partition1 - 1];
-        let minRight1 = partition1 === m ? Infinity : nums1[partition1];
-
-        let maxLeft2 = partition2 === 0 ? -Infinity : nums2[partition2 - 1];
-        let minRight2 = partition2 === n ? Infinity : nums2[partition2];
-
-        //Step3: Check that partitions are right or not
-        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-            //check that n+m length is odd or even 
-            if ((n + m) % 2 === 1) {
-                return Math.max(maxLeft1, maxLeft2);
-            } else {
-                return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+        if (l1 <= r2 && l2 <= r1) {
+            if ((n + m) % 2 == 1) {
+                return Math.max(l1, l2)
+            }
+            else {
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2
             }
         }
 
-        //Step4: Adjust the partitions
-        if (maxLeft1 > minRight2) {
-            end = partition1 - 1;
-        } else {
-            start = partition1 + 1;
+        if (l1 > r2) {
+            high = mid1 - 1
         }
-
-
+        else {
+            low = mid1 + 1
+        }
     }
+
 }
