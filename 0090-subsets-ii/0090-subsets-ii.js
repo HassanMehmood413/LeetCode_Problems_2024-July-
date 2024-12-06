fa9eebc503c9faea855841dbee00f78855d06b80
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-06 07:38:44 +0500

    Time: 0 ms (100%), Space: 54.5 MB (21.61%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -3,24 +3,44 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function (nums) {
-    let arr = []
-    let set = new Set()
-    nums = nums.sort((a, b) => a - b)
+    // let arr = []
+    // let set = new Set()
+    // nums = nums.sort((a, b) => a - b)
+    // var again = function (i) {
+    //     if (i >= nums.length) {
+    //         let key = JSON.stringify(arr);
+    //         if (!set.has(key)) {
+    //             set.add(key)
+    //         }
+    //         return
+    //     }
+
+    //     arr.push(nums[i])
+    //     again(i + 1)
+    //     arr.pop()
+    //     again(i + 1)
+    // }
+    // again(0)
+    // return Array.from(set).map(item => JSON.parse(item));
+
+
+    nums.sort((a, b) => a - b)
+    let res = []
+    let subset = []
     var again = function (i) {
         if (i >= nums.length) {
-            let key = JSON.stringify(arr);
-            if (!set.has(key)) {
-                set.add(key)
-            }
-            return
+            res.push([...subset])
+            return res
         }
-
-        arr.push(nums[i])
+        subset.push(nums[i])
         again(i + 1)
-        arr.pop()
+        subset.pop()
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++
+        }
         again(i + 1)
     }
     again(0)
-    return Array.from(set).map(item => JSON.parse(item));
+    return res
 
 };
