7b09748c10017394e8ef8f447c93b98839e52523
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-12 21:40:11 +0500

    Time: 36 ms (86.44%), Space: 17.4 MB (21.34%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -11,20 +11,23 @@ class Solution:
         if not root:
             return False
         queue = [root]
+
         while queue:
-            current = queue.pop(0) 
-            if current and current.val == subRoot.val and self.sametree(current, subRoot):
+            current = queue.pop(0)
+
+            if current.val == subRoot.val and self.sametree(current, subRoot):
                 return True
             if current:
                 if current.left:
                     queue.append(current.left)
                 if current.right:
                     queue.append(current.right)
-        
+
         return False
 
-    def sametree(self, root, subRoot):
+
+
+    def sametree(self,root,subRoot):
         if not root or not subRoot:
             return root == subRoot
-        return root.val == subRoot.val and self.sametree(root.left, subRoot.left) and self.sametree(root.right, subRoot.right)
-
+        return root.val == subRoot.val and self.sametree(root.left,subRoot.left) and self.sametree(root.right , subRoot.right)
