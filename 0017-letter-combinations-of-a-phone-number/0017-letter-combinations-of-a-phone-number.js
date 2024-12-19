de0b68fec35e16bbc5e1713d700a110ee06e678a
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-19 07:38:43 +0500

    Time: 0 ms (100%), Space: 48.3 MB (94.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -3,28 +3,30 @@
  * @return {string[]}
  */
 var letterCombinations = function (digits) {
-    const phoneMap = {
-        2: 'abc',
-        3: 'def',
-        4: 'ghi',
-        5: 'jkl',
-        6: 'mno',
-        7: 'qprs',
-        8: 'tuv',
-        9: 'wxyz',
-    };
+    keyboard = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+
     let res = []
-    var again = function (i, str, digits, phoneMap, res) {
+    var again = function (i, digits, keyboard, str) {
         if (str.length == digits.length) {
             res.push(str)
             return
         }
-        for (let s of phoneMap[digits[i]]) {
-            again(i + 1, str + s, digits, phoneMap, res)
+        for (let s of keyboard[digits[i]]) {
+            again(i + 1, digits, keyboard, str + s)
         }
     }
     if (digits) {
-        again(0, "", digits, phoneMap, res)
+        again(0, digits, keyboard, '')
     }
     return res
+
 };
