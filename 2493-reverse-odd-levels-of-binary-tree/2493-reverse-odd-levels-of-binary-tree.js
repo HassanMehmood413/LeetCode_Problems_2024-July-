a0a798a4b23d0abd0465a9b5ceb45cf8a03ff92c
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-20 09:24:01 +0500

    Time: 3 ms (78.79%), Space: 77.5 MB (6.06%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.js
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.js
@@ -11,34 +11,53 @@
  * @return {TreeNode}
  */
 var reverseOddLevels = function (root) {
-    if (!root) return []
-    let queue = [root]
-    let odd_count = 0
-    while (queue.length > 0) {
-        let collect = []
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            collect.push(first)
-            if (first.left) {
-                queue.push(first.left)
-            }
-            if (first.right) {
-                queue.push(first.right)
-            }
-        }
-        if (odd_count % 2 !== 0) {
-            let left = 0
-            let right = collect.length - 1
-            while (left < right) {
-                let temp = collect[left].val
-                collect[left].val = collect[right].val
-                collect[right].val = temp
-                left++
-                right--
-            }
-        }
-        odd_count++
-    }
+    // if (!root) return []
+    // let queue = [root]
+    // let odd_count = 0
+    // while (queue.length > 0) {
+    //     let collect = []
+    //     let length = queue.length
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         collect.push(first)
+    //         if (first.left) {
+    //             queue.push(first.left)
+    //         }
+    //         if (first.right) {
+    //             queue.push(first.right)
+    //         }
+    //     }
+    //     if (odd_count % 2 !== 0) {
+    //         let left = 0
+    //         let right = collect.length - 1
+    //         while (left < right) {
+    //             let temp = collect[left].val
+    //             collect[left].val = collect[right].val
+    //             collect[right].val = temp
+    //             left++
+    //             right--
+    //         }
+    //     }
+    //     odd_count++
+    // }
+    // return root
+
+
+    // Using dfs
+    if (!root) return;
+    dfs(root.left, root.right, 1)
     return root
+
+    function dfs(leftroot, rightroot, level) {
+        if (!leftroot || !rightroot) return;
+
+        if (level % 2 == 1) {
+            let temp = leftroot.val
+            leftroot.val = rightroot.val
+            rightroot.val = temp
+        }
+
+        dfs(leftroot.left, rightroot.right, level + 1)
+        dfs(leftroot.right, rightroot.left, level + 1)
+    }
 };  
