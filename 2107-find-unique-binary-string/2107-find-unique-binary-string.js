d39a8ef3720db3c2758b981ff4641d4aab03f17c
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-21 09:03:31 +0500

    Time: 0 ms (100%), Space: 48.9 MB (86.57%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.js
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.js
@@ -3,10 +3,31 @@
  * @return {string}
  */
 var findDifferentBinaryString = function (nums) {
-    let str = ''
-    let l = 0
-    for (let i = 0; i <= nums.length - 1; i++) {
-        str += nums[i][i] == '0' ? '1' : '0'
+    // let str = ''
+    // let l = 0
+    // for (let i = 0; i <= nums.length - 1; i++) {
+    //     str += nums[i][i] == '0' ? '1' : '0'
+    // }
+    // return str
+
+    var again = function (current) {
+        if (current.length == nums.length) {
+            if (!nums.includes(current)) {
+                return current
+            }
+            else {
+                return null
+            }
+        }
+
+
+        for (let digit in ['0', '1']) {
+            let result = again(current + digit)
+            if (result) {
+                return result
+            }
+        }
+        return null
     }
-    return str
+    return again('')
 };
