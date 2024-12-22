5c90393201f7baf5bb84222cba31522c3361cd93
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-22 18:45:14 +0500

    Time: 10 ms (70.42%), Space: 58.8 MB (15.49%) - LeetHub

--- 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.js
+++ 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.js
@@ -9,31 +9,49 @@
  * @param {ListNode} head
  * @return {number[]}
  */
+
 var nextLargerNodes = function (head) {
+    let answer = []
+    let stack = []
+    let count = 0
     let current = head
-    let next_node = current.next
-    let arr = []
-    while (current != null && next_node != null) {
-        if (next_node.val > current.val) {
-            arr.push(next_node.val)
+    while (current != null) {
+        answer.push(0)
+        while (stack.length && current.val > stack[stack.length - 1][1]) {
+            let [ct, value] = stack.pop()
+            answer[ct] = current.val
         }
-        else {
-            let val = again(next_node, current.val)
-            arr.push(val)
-        }
+        stack.push([count, current.val])
+        count++
         current = current.next
-        next_node = current.next
     }
-    arr.push(0)
-    return arr
-};
-var again = function (head, val) {
-    let current_full = head
-    while (current_full != null) {
-        if (current_full.val > val) {
-            return current_full.val
-        }
-        current_full = current_full.next
-    }
-    return 0
+    return answer
 }
+// var nextLargerNodes = function (head) {
+//     let current = head
+//     let next_node = current.next
+//     let arr = []
+//     while (current != null && next_node != null) {
+//         if (next_node.val > current.val) {
+//             arr.push(next_node.val)
+//         }
+//         else {
+//             let val = again(next_node, current.val)
+//             arr.push(val)
+//         }
+//         current = current.next
+//         next_node = current.next
+//     }
+//     arr.push(0)
+//     return arr
+// };
+// var again = function (head, val) {
+//     let current_full = head
+//     while (current_full != null) {
+//         if (current_full.val > val) {
+//             return current_full.val
+//         }
+//         current_full = current_full.next
+//     }
+//     return 0
+// }
