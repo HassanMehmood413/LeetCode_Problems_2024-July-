97ed4d1bed19ebd1323a11e9b0bc624aa906298b
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-22 18:13:49 +0500

    Time: 831 ms (5.63%), Space: 56.8 MB (95.77%) - LeetHub

---
+++ 1072-next-greater-node-in-linked-list/1072-next-greater-node-in-linked-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var nextLargerNodes = function (head) {
+    let current = head
+    let next_node = current.next
+    let arr = []
+    while (current != null && next_node != null) {
+        if (next_node.val > current.val) {
+            arr.push(next_node.val)
+            current = current.next
+            next_node = current.next
+        }
+        else {
+            let val = again(next_node, current.val)
+            arr.push(val)
+            current = current.next
+            next_node = current.next
+        }
+    }
+    arr.push(0)
+    return arr
+};
+var again = function (head, val) {
+    let current_full = head
+    while (current_full != null) {
+        if (current_full.val > val) {
+            return current_full.val
+        }
+        else {
+            current_full = current_full.next
+        }
+    }
+    return 0
+}
