95536e31c8ef491688a95167b594aa1ca8d1ac1d
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-27 05:39:59 +0500

    Time: 0 ms (100%), Space: 54.2 MB (80%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.js
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.js
@@ -3,18 +3,29 @@
  * @return {number}
  */
 var maxScoreSightseeingPair = function (values) {
-    let arr = []
-    let max = 0
-    for (let i = 0; i < values.length - 1; i++) {
-        let j = i + 1
-        while (j < values.length) {
-            let a = values[i] + values[j] + i - j
-            max = Math.max(a, max)
-            j++
-        }
-        if (i + 1 >= values.length - 1) {
-            arr.push(max)
-        }
+    // TLE
+
+    // let arr = []
+    // let max = 0
+    // for (let i = 0; i < values.length - 1; i++) {
+    //     let j = i + 1
+    //     while (j < values.length) {
+    //         let a = values[i] + values[j] + i - j
+    //         max = Math.max(a, max)
+    //         j++
+    //     }
+    //     if (i + 1 >= values.length - 1) {
+    //         arr.push(max)
+    //     }
+    // }
+    // return arr[0]
+
+    let max = -Infinity
+    let maxl = values[0] + 0
+    for (let j = 1; j < values.length; j++){
+        let a = maxl + values[j] - j
+        max = Math.max(max,a)
+        maxl = Math.max(maxl,values[j]+j)
     }
-    return arr[0]
+    return max
 };
