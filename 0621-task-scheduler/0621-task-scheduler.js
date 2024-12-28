8d941b6e29bfcc07f37dddcf890399fd7cb7043b
Hassan Mehmood <ihassan463m@gmail.com>
2024-12-28 08:38:55 +0500

    Time: 11 ms (76.24%), Space: 52.2 MB (99.68%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -4,19 +4,17 @@
  * @return {number}
  */
 var leastInterval = function (tasks, n) {
-    let x = 0
-    let fcount = 0
-    let totaljobs = tasks.length
     let map = new Map()
+    let count = 0
     let max = 0
     tasks.forEach((value) => {
-        map.set(value, (map.get(value) || 0) + 1);
+        map.set(value, (map.get(value) || 0) + 1)
         max = Math.max(max, map.get(value))
     })
-    for (let [a, b] of map) {
-        if (max == b) {
-            fcount++
+    for (let [a, v] of map) {
+        if (max == v) {
+            count++
         }
     }
-    return Math.max((n + 1) * (max - 1) + fcount, totaljobs)
+    return Math.max((n + 1) * (max - 1) + count, tasks.length)
 };
