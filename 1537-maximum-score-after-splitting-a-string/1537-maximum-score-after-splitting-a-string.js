312957cfe7281ba14be36f0f9d095bdf5f43285e
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-01 12:56:13 +0500

    Time: 2 ms (62.44%), Space: 50.4 MB (56.85%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.js
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.js
@@ -3,36 +3,55 @@
  * @return {number}
  */
 var maxScore = function (s) {
-    let left_zeros = s[0]
-    let right_ones = ''
-    let left_counts = 0
-    let right_counts = 0
+    // My Solution:
 
-    if (s[0] == '0') left_counts++
+    // let left_zeros = s[0]
+    // let right_ones = ''
+    // let left_counts = 0
+    // let right_counts = 0
 
-    let high = 0
+    // if (s[0] == '0') left_counts++
 
-    for (let i = 1; i < s.length; i++) {
-        if (s[i] == '1') {
-            right_counts++
-        }
-        right_ones = right_ones + s[i]
+    // let high = 0
+
+    // for (let i = 1; i < s.length; i++) {
+    //     if (s[i] == '1') {
+    //         right_counts++
+    //     }
+    //     right_ones = right_ones + s[i]
+    // }
+    // right_ones = right_ones.split('')
+
+    // while (right_ones.length > 0) {
+    //     let score = left_counts + right_counts
+    //     high = Math.max(high, score)
+
+    //     let a = right_ones.shift()
+
+    //     if (a == '0') {
+    //         left_counts++
+    //     }
+    //     else {
+    //         right_counts--
+    //     }
+    //     left_zeros = left_zeros + a
+    // }
+    // return high
+
+    let ones = 0, zeros = 0, high = 0
+    for (let el of s) {
+        if (el == '1') ones++
     }
-    right_ones = right_ones.split('')
-
-    while (right_ones.length > 0) {
-        let score = left_counts + right_counts
-        high = Math.max(high, score)
-
-        let a = right_ones.shift()
-
-        if (a == '0') {
-            left_counts++
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] == '1') {
+            zeros--
         }
         else {
-            right_counts--
+            ones++
         }
-        left_zeros = left_zeros + a
+        let score = zeros + ones
+        high = Math.max(score, high)
     }
     return high
+
 };
