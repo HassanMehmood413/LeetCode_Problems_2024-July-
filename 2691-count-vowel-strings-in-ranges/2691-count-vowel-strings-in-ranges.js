3fcff9f3faca9da6012249a731e4b8f55d90b243
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-02 08:35:36 +0500

    Time: 7 ms (83.67%), Space: 80.2 MB (20.41%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.js
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.js
@@ -6,6 +6,7 @@
 var vowelStrings = function (words, queries) {
     let ans = []
     vowels = ['a', 'e', 'i', 'o', 'u']
+    // Check vowels strings in the words
     for (let i = 0; i < words.length; i++) {
         let First_char = words[i][0]
         let Last_char = words[i][words[i].length - 1]
@@ -16,9 +17,11 @@ var vowelStrings = function (words, queries) {
             words[i] = 0
         }
     }
+    // To make summarization of vowels strings
     for (let i = 1; i < words.length; i++) {
         words[i] = words[i] + words[i - 1]
     }
+    // Baki da kaam athey kita hy :) 
     for (let i = 0; i < queries.length; i++) {
         let first = queries[i][0]
         let second = queries[i][1]
