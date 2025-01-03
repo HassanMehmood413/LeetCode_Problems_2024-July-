f9119a0ffc6e07f43ce97a7030e889c86ef17704
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-03 08:41:47 +0500

    Time: 10 ms (88.89%), Space: 61.6 MB (91.85%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var waysToSplitArray = function (nums) {
-    let total_sum = nums.reduce((a, v) => a + v, 0); 
-    let pre = 0; 
-    let count = 0; 
+    let total_sum = nums.reduce((a, b) => a + b, 0)
+    let count = 0
+    let pre = 0
 
-    for (let i = 0; i < nums.length - 1; i++) { 
-        pre += nums[i]; 
-        let other = total_sum - pre; 
-        if (pre >= other) count++; 
+    for (let i = 0; i < nums.length - 1; i++) {
+        pre += nums[i]
+        let diff = total_sum - pre
+        if (pre >= diff) {
+            count++
+        }
     }
-
-    return count;
+    return count
 
 };
