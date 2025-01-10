4f2753b307eb31ddffacb29a0ca591d93dc84737
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-10 17:00:32 +0500

    Time: 108 ms (60.71%), Space: 70.2 MB (60.71%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.js
+++ 0952-word-subsets/0952-word-subsets.js
@@ -4,38 +4,39 @@
  * @return {string[]}
  */
 var wordSubsets = function (words1, words2) {
-    let wordfreq = new Map();
-    let result = [];
+    let wordfreq = new Map()
+    let result = []
 
     for (let i = 0; i < words2.length; i++) {
-        let freq = new Map();
-        for (const char of words2[i]) {
-            freq.set(char, (freq.get(char) || 0) + 1);
+        let freq = new Map()
+        for (let words of words2[i]) {
+            freq.set(words, (freq.get(words) || 0) + 1)
         }
-        for (const [char, count] of freq.entries()) {
-            wordfreq.set(char, Math.max(wordfreq.get(char) || 0, count));
+
+        for (let [char, ind] of freq.entries()) {
+            wordfreq.set(char, Math.max(wordfreq.get(char) || 0, ind))
         }
     }
 
     for (let i = 0; i < words1.length; i++) {
-        let totalfrequency = new Map();
+        let totalfrequency = new Map()
 
-        for (const char of words1[i]) {
-            totalfrequency.set(char, (totalfrequency.get(char) || 0) + 1);
+        for (let j = 0; j < words1[i].length; j++) {
+            totalfrequency.set(words1[i][j], (totalfrequency.get(words1[i][j]) || 0) + 1)
         }
 
-        let valid = true;
-        for (const [char, count] of wordfreq.entries()) {
-            if ((totalfrequency.get(char) || 0) < count) {
-                valid = false;
-                break;
+        // Now check the main thing
+        let valid = true
+        for (let [char, ind] of wordfreq.entries()) {
+            if ((totalfrequency.get(char) || 0) < ind) {
+                valid = false
+                break
             }
         }
-
-        if (valid) {
-            result.push(words1[i]);
+        if (valid == true) {
+            result.push(words1[i])
         }
-    }
 
+    }
     return result
 };
