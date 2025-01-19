483080b1f70b07263e11da5e216a982553c1a1d1
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-19 10:33:12 +0500

    Time: 6 ms (95.77%), Space: 56.4 MB (49.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,72 +4,45 @@
  * @return {boolean}
  */
 var canFinish = function (numCourses, prerequisites) {
-    let graph = []
-    for (let i = 0; i < numCourses; i++) {
-        graph[i] = []
-    }
-    for (let [u, v] of prerequisites) {
-        graph[u].push(v)
-    }
+    let graph = new Map()
 
-    var bfs = function (graph, visit, start) {
-        visit[start] = true
-        stack[start] = true
-        for (let neighbor of graph[start]) {
-            if (!visit[neighbor]) {
-                if (bfs(graph, visit, neighbor)) return true
-            }
-            else if(stack[neighbor]) return true
+    var again = function (node1, node2, graph) {
+        if (!graph.has(node1)) {
+            graph.set(node1, [])
         }
-        stack[start] = false
-        return false
+        graph.get(node1).push(node2)
     }
 
+    for (let i = 0; i < prerequisites.length; i++) {
+        again(prerequisites[i][0], prerequisites[i][1], graph)
+    }
 
     let visit = new Array(numCourses).fill(false)
-    let stack = new Array(numCourses).fill(false)
+    let restack = new Array(numCourses).fill(false)
+
+    var dfs = function (node) {
+        if (restack[node]) return false
+        if (visit[node]) return true
+
+
+        visit[node] = true
+        restack[node] = true
+
+
+        for (let neighbor of graph.get(node) || []) {
+            if (!dfs(neighbor)) {
+                return false
+            }
+        }
+        restack[node] = false
+        return true
+    }
+
+
     for (let i = 0; i < numCourses; i++) {
-        if (!visit[i]) {
-            if (bfs(graph, visit, i)) return false
+        if (!dfs(i)) {
+            return false
         }
     }
     return true
-
-
-
-    // Time Limit exceed
-    // let graph = new Map()
-    // var again = function (graph, node1, to) {
-    //     if (!graph.has(node1)) {
-    //         graph.set(node1, [])
-    //     }
-    //     graph.get(node1).push(to)
-    // }
-
-    // for (let i = 0; i < prerequisites.length; i++) {
-    //     again(graph, prerequisites[i][0], prerequisites[i][1])
-    // }
-    // var visit = new Set()
-    // var bfs = function (node) {
-    //     if (visit.has(node)) return false
-    //     visit.add(node)
-    //     for (let neighbor of graph.get(node) || []) {
-    //         if (!bfs(neighbor)) return false
-    //     }
-    //     visit.delete(node)
-    //     return true
-    // }
-
-
-
-    // var connected = function (graph) {
-    //     for (let node of graph.keys()) {
-    //         if (!visit.has(node)) {
-    //             if (!bfs(node)) return false
-    //         }
-    //     }
-    //     return true
-    // }
-    // return connected(graph)
-
 };
