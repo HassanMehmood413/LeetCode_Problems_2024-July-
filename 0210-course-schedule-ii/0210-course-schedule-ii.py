9e979719abd61c7c08bf04300c758aabf5fd760a
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-21 13:30:26 +0500

    Time: 0 ms (100%), Space: 18.9 MB (62.84%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {}
+        result = []
+        queue = []
+        indegree = [0] * numCourses
+
+        def again(garph, first, second):
+            if not first in graph:
+                graph[first] = []
+
+            graph[first].append(second)
+            indegree[second] += 1
+
+        for first, second in prerequisites:
+            again(graph, first, second)
+
+        # checking point from where we start
+        for point in range(numCourses):
+            if indegree[point] == 0:
+                queue.append(point)
+
+        while len(queue) > 0:
+            first = queue.pop(0)
+            result.append(first)
+            for neighbor in graph.get(first, []):
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return result[::-1] if len(result) == numCourses else []
