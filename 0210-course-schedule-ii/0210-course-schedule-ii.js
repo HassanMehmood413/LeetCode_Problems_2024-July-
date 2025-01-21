ff55596a61923afca3ba6d56c61052375923dc35
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-21 13:18:24 +0500

    Time: 5 ms (94.59%), Space: 53.3 MB (95.87%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,20 +4,22 @@
  * @return {number[]}
  */
 var findOrder = function (numCourses, prerequisites) {
-
-    // Kahns Algorithm
-    let graph = new Array(numCourses)
-    for (let i = 0; i < numCourses; i++) {
-        graph[i] = []
-    }
+    let graph = new Map()
+    let result = []
     let indegree = new Array(numCourses).fill(0)
-    let visit = new Array(numCourses).fill(false)
-    let arr = []
     let queue = []
-    for (let [pre, crs] of prerequisites) {
-        graph[crs].push(pre)
-        indegree[pre]++
+    var again = function (graph, first, second) {
+        if (!graph.has(first)) {
+            graph.set(first, [])
+        }
+        graph.get(first).push(second)
+        indegree[second]++
     }
+
+    for (let i = 0; i < prerequisites.length; i++) {
+        again(graph, prerequisites[i][0], prerequisites[i][1])
+    }
+
 
     for (let i = 0; i < numCourses; i++) {
         if (indegree[i] == 0) {
@@ -27,13 +29,13 @@ var findOrder = function (numCourses, prerequisites) {
 
     while (queue.length > 0) {
         let first = queue.shift()
-        arr.push(first)
-        for (let neighbor of graph[first] || []) {
+        result.push(first)
+        for (let neighbor of graph.get(first) || []) {
             indegree[neighbor]--
             if (indegree[neighbor] == 0) {
                 queue.push(neighbor)
             }
         }
     }
-    return arr.length == numCourses ? arr : []
+    return result.length == numCourses ? result.reverse() : []
 };
