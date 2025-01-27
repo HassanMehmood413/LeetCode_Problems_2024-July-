7a1e83ceab2352a1e3d4371db22c84df0cac28c5
Hassan Mehmood <ihassan463m@gmail.com>
2025-01-27 07:57:08 +0500

    Time: 74 ms (50%), Space: 59.4 MB (86.67%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.js
+++ 1558-course-schedule-iv/1558-course-schedule-iv.js
@@ -5,47 +5,65 @@
  * @return {boolean[]}
  */
 var checkIfPrerequisite = function (numCourses, prerequisites, queries) {
-    let graph = new Map()
+    // Using DFS 
+    // let graph = new Map()
 
-    var again = function (graph, first, second) {
-        if (!graph.has(first)) {
-            graph.set(first, [])
-        }
-        graph.get(first).push(second)
-    }
+    // var again = function (graph, first, second) {
+    //     if (!graph.has(first)) {
+    //         graph.set(first, [])
+    //     }
+    //     graph.get(first).push(second)
+    // }
+
+    // for (let [u, v] of prerequisites) {
+    //     again(graph, u, v)
+    // }
+
+    // var dfs = function (first_val, second_val, graph, visit) {
+    //     if (first_val == second_val) return true
+    //     if (visit.has(first_val)) return false
+    //     visit.add(first_val)
+
+    //     for (let neighbor of graph.get(first_val) || []) {
+    //         if (dfs(neighbor, second_val, graph, visit)) {
+    //             return true
+    //         }
+    //     }
+    //     visit.delete(second_val)
+    //     return false
+    // }
+
+    // let res = []
+    // for (let [first, second] of queries) {
+    //     let visit = new Set()
+    //     let req = graph.get(first)
+    //     if (!req) {
+    //         res.push(false)
+    //     }
+    //     else if (dfs(first, second, graph, visit)) {
+    //         res.push(true)
+    //     }
+    //     else {
+    //         res.push(false)
+    //     }
+    // }
+    // return res
+
+    // Using Floyd Warshall Algorithm
+    let reach = Array.from({ length: numCourses }, () => new Array(numCourses).fill(false))
 
     for (let [u, v] of prerequisites) {
-        again(graph, u, v)
+        reach[u][v] = true
     }
 
-
-    var dfs = function (first_val, second_val, graph, visit) {
-        if (first_val == second_val) return true
-        if (visit.has(first_val)) return false
-        visit.add(first_val)
-
-        for (let neighbor of graph.get(first_val) || []) {
-            if (dfs(neighbor, second_val, graph, visit)) {
-                return true
+    for (let k = 0; k < numCourses; k++) {
+        for (let i = 0; i < numCourses; i++) {
+            for (let j = 0; j < numCourses; j++) {
+                if (reach[i][k] && reach[k][j]) {
+                    reach[i][j] = true
+                }
             }
         }
-        visit.delete(second_val)
-        return false
     }
-
-    let res = []
-    for (let [first, second] of queries) {
-        let visit = new Set()
-        let req = graph.get(first)
-        if (!req) {
-            res.push(false)
-        }
-        else if (dfs(first, second, graph, visit)) {
-            res.push(true)
-        }
-        else {
-            res.push(false)
-        }
-    }
-    return res
+    return queries.map(([first, second]) => reach[first][second])
 };
