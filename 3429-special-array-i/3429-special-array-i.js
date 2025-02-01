8e4db225486371ce78a8c479007d3bd2af7997c7
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-01 11:28:02 +0500

    Time: 1 ms (64.19%), Space: 51.1 MB (92.09%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.js
+++ 3429-special-array-i/3429-special-array-i.js
@@ -3,12 +3,11 @@
  * @return {boolean}
  */
 var isArraySpecial = function (nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        let j = i + 1
-        if (nums[i] % 2 == 0 && nums[j] % 2 == 0) {
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] % 2 == 0 && nums[i - 1] % 2 == 0) {
             return false
         }
-        else if (nums[i] % 2 != 0 && nums[j] % 2 != 0) {
+        else if (nums[i] % 2 !== 0 && nums[i - 1] % 2  !== 0) {
             return false
         }
     }
