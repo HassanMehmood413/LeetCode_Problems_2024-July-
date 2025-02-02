5810200a021354ebb6664ae7fed388d74b2d2690
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-02 06:28:58 +0500

    Time: 0 ms (100%), Space: 48.9 MB (70.13%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.js
@@ -3,24 +3,41 @@
  * @return {boolean}
  */
 var check = function (nums) {
-    let copy1 = [...nums]
-    copy1 = copy1.sort((a, b) => a - b)
-    copy1 = copy1.join(',')
-    if (nums.join(',') == copy1) return true
-    console.log(copy1)
-    for (let i = 0; i < nums.length - 1; i++) {
-        let first = nums[i]
-        let second = nums[i + 1]
+    // let copy1 = [...nums]
+    // copy1 = copy1.sort((a, b) => a - b)
+    // copy1 = copy1.join(',')
+    // if (nums.join(',') == copy1) return true
+    // console.log(copy1)
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     let first = nums[i]
+    //     let second = nums[i + 1]
 
-        if (first > second) {
-            let a = nums.slice(0, i + 1)
-            let b = nums.slice(i + 1, nums.length)
-            console.log(a, b)
-            let new_a = b.concat(a)
-            if (new_a.join(',') == copy1) {
-                return true
-            }
+    //     if (first > second) {
+    //         let a = nums.slice(0, i + 1)
+    //         let b = nums.slice(i + 1, nums.length)
+    //         console.log(a, b)
+    //         let new_a = b.concat(a)
+    //         if (new_a.join(',') == copy1) {
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false
+
+    let rp = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i + 1] < nums[i]) {
+            rp++
         }
     }
-    return false
+    if (rp == 0) return true
+    else if (rp == 1) {
+        if (nums[0] >= nums[nums.length - 1]) return true
+        else {
+            return false
+        }
+    }
+    else {
+        return false
+    }
 };
