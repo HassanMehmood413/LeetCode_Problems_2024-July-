80289702e857c11fcd9c6b5d10adbc13b0a4a4e2
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-05 08:16:45 +0500

    Time: 1 ms (21.62%), Space: 50.4 MB (19.82%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
@@ -4,27 +4,25 @@
  * @return {boolean}
  */
 var areAlmostEqual = function (s1, s2) {
-    if (s1 == s2 && s1.length == s2.length) {
-        return true
-    }
-    let arr = []
-    for (let i = 0; i < s1.length; i++) {
-        if (s1[i] !== s2[i]) {
-            arr.push(i)
-        }
-    }
+    if (s1 == s2) return true
+    if (s1.length != s2.length) return false
+    let count = 0
+    let map1 = new Map();
+    let map2 = new Map();
+    s1 = s1.split('')
     s2 = s2.split('')
-    // for (let i = 0; i < arr.length; i += 2) {
-    let temp = s2[arr[0]]
-    s2[arr[0]] = s2[arr[0 + 1]]
-    s2[arr[0 + 1]] = temp
-    // }
-    s2 = s2.join('')
-    if (s2 == s1) {
-        return true
+    s1.forEach((num) => {
+        map1.set(num, (map1.get(num) || 0) + 1);
+    });
+    s2.forEach((num) => {
+        map2.set(num, (map2.get(num) || 0) + 1);
+    });
+    for (let i = 0; i < s1.length; i++) {
+        let first = map1.get(s1[i])
+        let second = map2.get(s1[i])
+        if (first !== second) return false
+        if (s1[i] != s2[i]) count++
     }
-    else {
-        return false
-    }
-
-};
+    if (count == 2) return true
+    return false
+}
