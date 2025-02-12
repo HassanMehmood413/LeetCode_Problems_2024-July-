c69aaf994000af6720445534c107d4f2447088fc
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-12 07:42:04 +0500

    Time: 26 ms (95.29%), Space: 62.7 MB (60%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -3,24 +3,39 @@
  * @return {number}
  */
 var maximumSum = function (nums) {
-    let arr = []
-    let map = new Map()
-    for (let i = 0; i < nums.length; i++) {
-        let number = nums[i]
-        sum = sumOfDigits(number)
-        if (!map.has(sum)) {
-            map.set(sum, [])
-        }
-        map.get(sum).push(number)
-    }
-    let maxsum = -1
-    for (let values of map.values()) {
-        if (values.length > 1) {
-            values.sort((a, b) => b - a); 
-            maxsum = Math.max(maxsum, values[0] + values[1])
-        }
-    }
+    // let arr = []
+    // let map = new Map()
+    // for (let i = 0; i < nums.length; i++) {
+    //     let number = nums[i]
+    //     sum = sumOfDigits(number)
+    //     if (!map.has(sum)) {
+    //         map.set(sum, [])
+    //     }
+    //     map.get(sum).push(number)
+    // }
+    // let maxsum = -1
+    // for (let values of map.values()) {
+    //     if (values.length > 1) {
+    //         values.sort((a, b) => b - a); 
+    //         maxsum = Math.max(maxsum, values[0] + values[1])
+    //     }
+    // }
 
+    // return maxsum
+
+    let maxsum = -1
+    let map = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        let digitsum = sumOfDigits(nums[i])
+        if (map[digitsum]) {
+            maxsum = Math.max(maxsum, map[digitsum] + nums[i])
+            map[digitsum] = Math.max(map[digitsum], nums[i])
+        }
+        else {
+            map[digitsum] = nums[i]
+        }
+    }
     return maxsum
 
 };
@@ -28,8 +43,8 @@ var maximumSum = function (nums) {
 function sumOfDigits(n) {
     let total = 0;
     while (n > 0) {
-        total += n % 10;  
-        n = Math.floor(n / 10); 
+        total += n % 10;
+        n = Math.floor(n / 10);
     }
     return total;
 }
