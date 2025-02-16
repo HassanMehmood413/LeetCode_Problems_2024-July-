020af69903fc701aed11381bde669558c82467a5
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-16 08:22:42 +0500

    Time: 6 ms (50%), Space: 49.4 MB (50%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.js
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.js
@@ -7,31 +7,22 @@ var constructDistancedSequence = function (n) {
     let new_arr = new Array(len).fill(0)
     let set = new Set()
     var backtrack = function (i) {
-        if (i == len) { return true }
+        if (i == len) return true
 
         if (new_arr[i] != 0) return backtrack(i + 1)
 
         for (let j = n; j >= 1; j--) {
-            if (set.has(j)) {
-                continue
-            }
-            if (j > 1 && (i + j >= len || new_arr[i + j] != 0)) {
-                continue
-            }
+
+            if (set.has(j)) continue
+            if (j > 1 && (i + j >= len || new_arr[i + j] != 0)) continue
+
             set.add(j)
             new_arr[i] = j
             if (j > 1) {
                 new_arr[i + j] = j
             }
 
-            // let k = i + 1
-            // while (k > new_arr.length && new_arr[k]) {
-            //     k = k + 1
-            // }
-
-            if (backtrack(i + 1)) {
-                return true
-            }
+            if (backtrack(i + 1)) return true
 
             set.delete(j)
             new_arr[i] = 0
