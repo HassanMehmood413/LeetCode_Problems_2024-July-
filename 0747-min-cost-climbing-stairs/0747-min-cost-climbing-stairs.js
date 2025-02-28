ff1f5fa7c2d6b7222c509cda97d126727818d384
Hassan Mehmood <ihassan463m@gmail.com>
2025-02-28 07:27:15 +0500

    Time: 1 ms (77.43%), Space: 53.9 MB (12.37%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,21 +3,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    var stairs = function (cost, n, dp) {
-        if (n == 0) {
-            return cost[0]
-        }
-        if (n == 1) {
-            return cost[1]
-        }
-        if (dp[n] != -1) {
-            return dp[n]
-        }
-        dp[n] = cost[n] + Math.min(stairs(cost, n - 1, dp), stairs(cost, n - 2, dp))
-        return dp[n]
+    let dp = new Array(cost.length).fill(-1)
+    var again = function (i) {
+        if (i >= cost.length) return 0
+        if (dp[i] != -1) return dp[i]
+        dp[i] = cost[i] + Math.min(again(i + 1), again(i + 2))
+        return dp[i]
     }
-    let n = cost.length
-    let dp = new Array(n + 1).fill(-1)
-    let ans = Math.min(stairs(cost, n - 1, dp), stairs(cost, n - 2, dp))
-    return ans
+    return Math.min(again(0), again(1))
 };
