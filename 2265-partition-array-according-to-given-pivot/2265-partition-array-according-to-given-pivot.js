047391a4ca4357dc0384cf0124687d5d7787c3ee
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-03 06:23:50 +0500

    Time: 15 ms (91.71%), Space: 81.8 MB (81.22%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
@@ -4,19 +4,23 @@
  * @return {number[]}
  */
 var pivotArray = function (nums, pivot) {
-    let first = []
-    let second = []
-    let last = []
+    let less_el = []
+    let big_el = []
+    let i = 0, j = 0;
+    let count_pivot = 0
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > pivot) {
-            first.push(nums[i])
+        if (nums[i] == pivot) count_pivot++
+        if (nums[i] < pivot) {
+            less_el.push(nums[i])
         }
-        else if (nums[i] < pivot) {
-            last.push(nums[i])
-        }
-        else {
-            second.push(nums[i])
+        else if (nums[i] > pivot) {
+            big_el.push(nums[i])
         }
     }
-    return last.concat(second,first)
+    while (count_pivot > 0) {
+        less_el.push(pivot)
+        count_pivot--
+    }
+    return less_el.concat(big_el)
+
 };
