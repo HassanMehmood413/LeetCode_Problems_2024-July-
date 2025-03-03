3959cc5d0b347a691110b260a47f20170b0e149d
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-03 06:28:20 +0500

    Time: 28 ms (70.17%), Space: 87.3 MB (14.36%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.js
@@ -4,23 +4,41 @@
  * @return {number[]}
  */
 var pivotArray = function (nums, pivot) {
-    let less_el = []
-    let big_el = []
-    let i = 0, j = 0;
-    let count_pivot = 0
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == pivot) count_pivot++
-        if (nums[i] < pivot) {
-            less_el.push(nums[i])
-        }
-        else if (nums[i] > pivot) {
-            big_el.push(nums[i])
-        }
+    // let less_el = []
+    // let big_el = []
+    // let i = 0, j = 0;
+    // let count_pivot = 0
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] == pivot) count_pivot++
+    //     if (nums[i] < pivot) {
+    //         less_el.push(nums[i])
+    //     }
+    //     else if (nums[i] > pivot) {
+    //         big_el.push(nums[i])
+    //     }
+    // }
+    // while (count_pivot > 0) {
+    //     less_el.push(pivot)
+    //     count_pivot--
+    // }
+    // return less_el.concat(big_el)
+    let res = new Array(nums.length)
+    let left = 0, right = 0, count_pivot = 0
+
+    for (let num of nums) {
+        if (num == pivot) count_pivot++
+        if (num < pivot) res[left++] = num
     }
+
     while (count_pivot > 0) {
-        less_el.push(pivot)
+        res[left++] = pivot
         count_pivot--
     }
-    return less_el.concat(big_el)
 
+    for (let num of nums) {
+        if (num > pivot) {
+            res[left++] = num
+        }
+    }
+    return res
 };
