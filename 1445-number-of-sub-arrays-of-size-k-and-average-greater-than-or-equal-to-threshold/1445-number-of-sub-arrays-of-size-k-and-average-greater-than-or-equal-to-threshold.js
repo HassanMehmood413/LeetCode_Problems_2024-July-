72f9964672a4999b17348a9419ab88945f25c401
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-11 06:05:08 +0500

    Time: 2 ms (73.6%), Space: 62.4 MB (16.4%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -5,24 +5,41 @@
  * @return {number}
  */
 var numOfSubarrays = function (arr, k, threshold) {
-    let check = []
+    // let check = []
+    // let count = 0
+    // let sum = 0
+    // let left = 0
+    // let right = 0
+    // while (right < arr.length) {
+    //     check.push(arr[right])
+    //     sum = sum + arr[right]
+    //     if (check.length == k) {
+    //         let avg = sum / k
+    //         if (avg >= threshold) count++
+    //     }
+    //     while (check.length >= k) {
+    //         check.shift()
+    //         sum -= arr[left]
+    //         left++
+    //     }
+    //     right++
+    // }
+    // return count
+
+
     let count = 0
     let sum = 0
-    let left = 0
-    let right = 0
-    while (right < arr.length) {
-        check.push(arr[right])
-        sum = sum + arr[right]
-        if (check.length == k) {
+    let l = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+
+        if (i - l + 1 >= k) {
             let avg = sum / k
             if (avg >= threshold) count++
+            sum -= arr[l]
+            l++
         }
-        while (check.length >= k) {
-            check.shift()
-            sum -= arr[left]
-            left++
-        }
-        right++
     }
     return count
 };
