d8b28ef92ef5acf8ae6fafe24281b9b53f475979
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-12 21:17:45 +0500

    Time: 3 ms (16.22%), Space: 54.3 MB (35.35%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,23 +3,13 @@
  * @return {number}
  */
 var maximumCount = function (nums) {
-    let count = 0
-    let neg = 0
     let pos = 0
+    let neg = 0
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < 0) {
-            neg++
-        }
+        if (nums[i] < 0) neg++
         else if (nums[i] > 0) {
             pos++
         }
     }
-    if (neg > pos) {
-        count = neg
-    }
-    else {
-        count = pos
-    }
-    return count
-
+    return Math.max(pos, neg)
 };
