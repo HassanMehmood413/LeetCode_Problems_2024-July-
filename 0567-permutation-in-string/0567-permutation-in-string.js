bd03de97507d8ef6f12d9590015a6b1b8d9ac8fc
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-13 07:12:30 +0500

    Time: 31 ms (43.29%), Space: 60 MB (20.68%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,41 +4,41 @@
  * @return {boolean}
  */
 var checkInclusion = function (s1, s2) {
-    if (s2.length < s1.length) return false
-    let f1 = new Map()
-    let f2 = new Map()
+    let str = ''
+    s1 = s1.split('')
+    let check = false
+    let left = 0; right = 0
+    let map1 = new Map()
+    let map2 = new Map()
+
     for (let i = 0; i < s1.length; i++) {
-        f1.set(s1[i], (f1.get(s1[i]) || 0) + 1)
-        f2.set(s2[i], (f2.get(s2[i]) || 0) + 1)
+        map1.set(s1[i], (map1.get(s1[i]) || 0) + 1)
+        map2.set(s2[i], (map2.get(s2[i]) || 0) + 1)
     }
+    console.log(map1, map2)
 
-    if (Equal(f1, f2)) {
-        return true
-    }
+    if (Equal(map1, map2)) return true
 
-    // Sliding Window
     for (let i = s1.length; i < s2.length; i++) {
         let cout = s2[i - s1.length]
-        if (f2.get(cout) == 1) {
-            f2.delete(cout)
+        if (map2.get(cout) == 1) {
+            map2.delete(cout)
         }
         else {
-            f2.set(cout, f2.get(cout) - 1)
+            map2.set(cout, map2.get(cout) - 1)
         }
         let cin = s2[i]
-        f2.set(cin, (f2.get(cin) || 0) +
-            1)
-
-        if (Equal(f1, f2)) return true
-
+        map2.set(cin, (map2.get(cin) || 0) + 1)
+        if (Equal(map1, map2)) return true
     }
     return false
-}
+
+};
 
 var Equal = function (map1, map2) {
-    if (map1.size != map2.size) return false
+    if (map1.size !== map2.size) return false
     for (let [key, value] of map1) {
-        if (value != map2.get(key)) return false
+        if (value !== map2.get(key)) return false
     }
     return true
 }
