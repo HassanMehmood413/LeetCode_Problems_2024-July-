543b6d33684812372048c8594d83ad24104935ba
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-14 06:10:19 +0500

    Time: 42 ms (41.94%), Space: 73.5 MB (6.45%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.js
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 var maximumCandies = function (candies, k) {
-    candies = candies.sort((a, b) => a - b)
     let sum = candies.reduce((a, b) => a + b, 0)
     if (sum < k) return 0
     let left = 1
-    let right = Math.floor(sum / k)
+    let right = Math.max(...candies)
     let result = 0
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
         let count = 0
+
         for (let candy of candies) {
             count += Math.floor(candy / mid)
-            if (count >= k) break;
         }
+
         if (count >= k) {
             left = mid + 1
             result = mid
