55a8221185db7111f99b064160205e108845911e
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-19 05:44:41 +0500

    Time: 6 ms (80%), Space: 65.1 MB (20%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -3,17 +3,15 @@
  * @return {number}
  */
 var minOperations = function (nums) {
-    let flips = 0
+    let flip = 0
     for (let i = 0; i < nums.length - 2; i++) {
         if (nums[i] == 0) {
-            flips++
+            flip++
             nums[i] = 1 - nums[i]
             nums[i + 1] = 1 - nums[i + 1]
             nums[i + 2] = 1 - nums[i + 2]
         }
     }
-    if (nums[nums.length - 1] == 0 || nums[nums.length - 2] == 0) {
-        return -1
-    }
-    return flips
+    if (nums[nums.length - 1] == 0 || nums[nums.length - 2] == 0) return -1
+    return flip
 };
