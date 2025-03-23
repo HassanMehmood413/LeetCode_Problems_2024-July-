e450ccac75c4276386cba69db291310090f5c259
Hassan Mehmood <ihassan463m@gmail.com>
2025-03-23 17:08:10 +0500

    Time: 2 ms (76.2%), Space: 54.5 MB (47.5%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,22 +4,17 @@
  */
 var isValid = function (s) {
     let stack = []
-    let st = "([{"
-    if(s.length % 2 !== 0) return false
+    let str = '([{'
+    if (s.length % 2 != 0) return false
     for (let i = 0; i < s.length; i++) {
-        let char = s[i]
-
-        if (st.includes(char)) {
-            stack.push(s[i])
+        let first = s[i]
+        if (str.includes(first)) {
+            stack.push(first)
         }
         else {
             if (stack.length == 0) return false
-
             let top = stack.pop()
-
-            if ((top !== '(' && char == ')') || (top !== '[' && char == ']') || (top !== '{' && char == '}')) {
-                return false
-            }
+            if (top == '(' && first !== ')' || top == '[' && first != ']' || top == '{' && first != '}') return false
         }
     }
     return stack.length == 0
