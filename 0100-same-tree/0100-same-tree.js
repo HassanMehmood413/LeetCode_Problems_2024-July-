e31313c3710397391233c86374068db1b180bffb
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-24 04:50:43 +0500

    Time: 0 ms (100%), Space: 53.5 MB (70.68%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,28 +12,22 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-    // Using Recursion
-    // if (!p && !q) return true
-    // if (p && q && p.val == q.val) {
-    //     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
-    // }
-    // return false
-
-    if (!p && !q) return true
-    if (!p || !q) return false
-    let queue = [p]
+    let queue1 = [p]
     let queue2 = [q]
-    while (queue.length > 0 && queue2.length > 0) {
-        let first = queue.shift()
-        let second = queue2.shift()
-        if (!first && !second) continue
-        if (!first || !second) return false
-        if (first.val != second.val) return false
-        queue.push(first.left)
-        queue.push(first.right)
-        queue2.push(second.left)
-        queue2.push(second.right)
-    } return queue.length == 0 && queue2.length == 0
+    while (queue1.length && queue2.length) {
+        let length = queue1.length
+        for (let i = 0; i < length; i++) {
+            let first = queue1.shift()
+            let second = queue2.shift()
+            if(!first && !second) continue
+            if(!first || !second) return false
+            if(first.val !== second.val) return false
+            queue1.push(first.left)
+            queue2.push(second.left)
+            queue1.push(first.right)
+            queue2.push(second.right)
+        }
+    }
+    return true
 
-
-};   
+};
