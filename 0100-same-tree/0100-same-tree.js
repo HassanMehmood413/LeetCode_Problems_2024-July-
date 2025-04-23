e23110f6ad28f0236e0f095c26ca3bd4a3b60858
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-24 04:56:18 +0500

    Time: 0 ms (100%), Space: 54.1 MB (26.2%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,22 +12,32 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-    let queue1 = [p]
-    let queue2 = [q]
-    while (queue1.length && queue2.length) {
-        let length = queue1.length
-        for (let i = 0; i < length; i++) {
-            let first = queue1.shift()
-            let second = queue2.shift()
-            if(!first && !second) continue
-            if(!first || !second) return false
-            if(first.val !== second.val) return false
-            queue1.push(first.left)
-            queue2.push(second.left)
-            queue1.push(first.right)
-            queue2.push(second.right)
-        }
+    // let queue1 = [p]
+    // let queue2 = [q]
+    // while (queue1.length && queue2.length) {
+    //     let length = queue1.length
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue1.shift()
+    //         let second = queue2.shift()
+    //         if(!first && !second) continue
+    //         if(!first || !second) return false
+    //         if(first.val !== second.val) return false
+    //         queue1.push(first.left)
+    //         queue2.push(second.left)
+    //         queue1.push(first.right)
+    //         queue2.push(second.right)
+    //     }
+    // }
+    // return true
+
+    var again = function (p, q) {
+        if (!p && !q) return true
+        if (!p || !q) return false
+        if (p.val !== q.val) return false
+
+
+        return again(p.left , q.left) && again(p.right , q.right)
     }
-    return true
+    return again(p,q)
 
 };
