d22957799adaf3348a73813247b3571adee1c9d2
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-25 06:30:50 +0500

    Time: 1 ms (62.47%), Space: 63.7 MB (48.26%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -12,14 +12,33 @@
  * @return {TreeNode}
  */
 var mergeTrees = function (root1, root2) {
-    if (!root1 && !root2) {
-        return null
-    }
-    let val1 = root1 ? root1.val : 0;
-    let val2 = root2 ? root2.val : 0;
-    let root = new TreeNode(val1 + val2)
+    // let queue1 = [root1]
+    // let queue2 = [root2]
+    // while (queue1.length || queue2.length) {
+    //     let length = queue2.shift()
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue1.shift()
+    //         let second = queue2.shift()
+    //         let root = new TreeNode(first.val + second.val)
+    //         if (first.left && second.left) {
+    //             queue1.push(first.left)
+    //             queeu2.push(first.left)
+    //         }
+    //         if (first.right && second.right) {
+    //             queue1.push(first.right)
+    //             queue2.push(second.right)
+    //         }
 
-    root.left = mergeTrees(root1 ? root1.left : null, root2 ? root2.left : null)
-    root.right = mergeTrees(root1 ? root1.right : null, root2 ? root2.right : null)
-    return root
+    //     }
+    // }
+    var again = function (root1, root2) {
+        // if (!root1 && !root2) return true
+        if (!root1 || !root2) return root1 || root2
+
+        let root = new TreeNode(root1.val + root2.val)
+        root.left = again(root1.left, root2.left)
+        root.right = again(root1.right, root2.right)
+        return root
+    }
+    return again(root1,root2)
 };
