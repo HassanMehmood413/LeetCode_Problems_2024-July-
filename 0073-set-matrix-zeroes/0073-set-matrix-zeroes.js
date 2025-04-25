8e2bce193ddaed98de5e4e9898bfdd07b382e664
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-26 04:59:42 +0500

    Time: 5 ms (32.95%), Space: 59.7 MB (22.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,27 +3,24 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-    // check columns 
-    let columns = []
+    // for rows
+    let col = []
     for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j] == 0) {
-                columns.push(j)
+        if (matrix[i].includes(0)) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    col.push(j)
+                }
+                matrix[i][j] = 0
             }
         }
     }
-    console.log(columns)
+
     for (let i = 0; i < matrix.length; i++) {
-        if (matrix[i].includes(0)) {
-            matrix[i].fill(0)
-        }
-    }
-
-    for (let i = 0; i < columns.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
-            matrix[j][columns[i]] = 0
-
+        for (let j of col) {
+            matrix[i][j] = 0
         }
     }
     return matrix
+
 };
