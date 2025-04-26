e4d10e1f14d73b8e832ca2aaaf9bf647ed58103d
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-26 06:42:42 +0500

    Time: 0 ms (100%), Space: 54.7 MB (40.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,26 +3,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function (nums) {
-    // let i = nums.length - 2
-    // while (i >= 0 && nums[i] >= nums[i + 1]) {
-    //     i--
-    // }
-    // if (i >= 0) {
-    //     let j = nums.length - 1
-    //     while (nums[j] <= nums[i]) {
-    //         j--
-    //     }
-    //     [nums[i], nums[j]] = [nums[j], nums[i]]
-    // }
-    // let left = i + 1
-    // let right = nums.length - 1
-    // while (left < right) {
-    //     [nums[left], nums[right]] = [nums[right], nums[left]]
-    //     left++
-    //     right--
-    // }
-    // return nums
-
+    // Finding lexigraphicall order
 
     // Find the first smallest number
     // Find the first number greater than the smallest one
@@ -34,7 +15,6 @@ var nextPermutation = function (nums) {
         i--
     }
 
-    // Now swap
     if (i >= 0) {
         let j = nums.length - 1
         while (nums[j] <= nums[i]) {
@@ -42,6 +22,7 @@ var nextPermutation = function (nums) {
         }
         [nums[i], nums[j]] = [nums[j], nums[i]]
     }
+
     let first = i + 1
     let last = nums.length - 1
     while (first < last) {
