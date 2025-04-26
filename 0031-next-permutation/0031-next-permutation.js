abe6d6a5cd5173c9cd2b58eca0f0da3b148f333a
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-26 06:36:35 +0500

    Time: 0 ms (100%), Space: 54.1 MB (81.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,10 +3,38 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function (nums) {
+    // let i = nums.length - 2
+    // while (i >= 0 && nums[i] >= nums[i + 1]) {
+    //     i--
+    // }
+    // if (i >= 0) {
+    //     let j = nums.length - 1
+    //     while (nums[j] <= nums[i]) {
+    //         j--
+    //     }
+    //     [nums[i], nums[j]] = [nums[j], nums[i]]
+    // }
+    // let left = i + 1
+    // let right = nums.length - 1
+    // while (left < right) {
+    //     [nums[left], nums[right]] = [nums[right], nums[left]]
+    //     left++
+    //     right--
+    // }
+    // return nums
+
+
+    // Find the first smallest number
+    // Find the first number greater than the smallest one
+    // swap them 
+    // and reverse the array after the first smallest number
+
     let i = nums.length - 2
     while (i >= 0 && nums[i] >= nums[i + 1]) {
         i--
     }
+
+    // Now swap
     if (i >= 0) {
         let j = nums.length - 1
         while (nums[j] <= nums[i]) {
@@ -14,12 +42,15 @@ var nextPermutation = function (nums) {
         }
         [nums[i], nums[j]] = [nums[j], nums[i]]
     }
-    let left = i + 1
-    let right = nums.length - 1
-    while (left < right) {
-        [nums[left], nums[right]] = [nums[right], nums[left]]
-        left++
-        right--
+    let first = i + 1
+    let last = nums.length - 1
+    while (first < last) {
+        [nums[first], nums[last]] = [nums[last], nums[first]]
+        first++
+        last--
     }
     return nums
+
+
+
 };
