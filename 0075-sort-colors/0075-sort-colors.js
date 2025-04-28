1eb4b67e39d003b7f29d0e8ba18920fe828a9932
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-28 05:28:41 +0500

    Time: 0 ms (100%), Space: 53.8 MB (56.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,55 +3,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    // let left = 0, mid = 0, right = nums.length - 1
-    // while (mid <= right) {
-    //     if (nums[mid] == 0) {
-    //         [nums[left], nums[mid]] = [nums[mid], nums[left]]
-    //         left++
-    //         mid++
-    //     }
-    //     else if (nums[mid] == 1) {
-    //         mid++
-    //     }
-    //     else {
-    //         [nums[mid], nums[right]] = [nums[right], nums[mid]]
-    //         right--
-    //     }
-    // } return nums
-
-    comp(nums, 0, nums.length - 1)
-    return nums
-};
-function comp(nums, start, end) {
-    if (start >= end) return 0
-    let mid = Math.floor((start + end) / 2)
-    comp(nums, start, mid)
-    comp(nums, mid + 1, end)
-    merge(nums, start, mid, end)
-}
-function merge(nums, start, mid, end) {
-    let left = start
-    let right = mid + 1
-    let temp = []
-    while (left <= mid && right <= end) {
-        if (nums[left] <= nums[right]) {
-            temp.push(nums[left])
+    let left = 0
+    let right = nums.length - 1
+    let index = 0
+    while (index <= right) {
+        if (nums[index] == 0) {
+            [nums[index], nums[left]] = [nums[left], nums[index]]
             left++
+            index++
+        }
+        else if (nums[index] == 2) {
+            [nums[index], nums[right]] = [nums[right], nums[index]]
+            right = right - 1
         }
         else {
-            temp.push(nums[right])
-            right++
+            index++
         }
     }
-    while (left <= mid) {
-        temp.push(nums[left])
-        left++
-    }
-    while (right <= end) {
-        temp.push(nums[right])
-        right++
-    }
-    for (let i = start; i <= end; i++) {
-        nums[i] = temp[i - start]
-    }
-}
+    return nums
+};
