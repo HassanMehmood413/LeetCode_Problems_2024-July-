87c1313b2513bf3fe9a13177ff4d90976ef7d65f
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-30 04:54:17 +0500

    Time: 2 ms (30.07%), Space: 59.5 MB (34.73%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -3,6 +3,8 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function (nums) {
+    // Both methods work correct 
+
     // let arr = []
     // let set = new Set()
     // nums = nums.sort((a, b) => a - b)
@@ -24,23 +26,41 @@ var subsetsWithDup = function (nums) {
     // return Array.from(set).map(item => JSON.parse(item));
 
 
-    nums.sort((a, b) => a - b)
+    // nums.sort((a, b) => a - b)
+    // let res = []
+    // let subset = []
+    // var again = function (i) {
+    //     if (i >= nums.length) {
+    //         res.push([...subset])
+    //         return res
+    //     }
+    //     subset.push(nums[i])
+    //     again(i + 1)
+    //     subset.pop()
+    //     while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+    //         i++
+    //     }
+    //     again(i + 1)
+    // }
+    // again(0)
+    // return res
+
     let res = []
-    let subset = []
-    var again = function (i) {
-        if (i >= nums.length) {
-            res.push([...subset])
-            return res
+    let set = new Set()
+    nums = nums.sort((a, b) => a - b)
+    var again = function (index, arr, set) {
+        let key = JSON.stringify(arr);
+        if (!set.has(key)) {
+            set.add(key)
+            res.push([...arr])
         }
-        subset.push(nums[i])
-        again(i + 1)
-        subset.pop()
-        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-            i++
+        for (let i = index; i < nums.length; i++) {
+            arr.push(nums[i])
+            again(i + 1, arr, set)
+            arr.pop()
         }
-        again(i + 1)
     }
-    again(0)
+    again(0, [], set)
     return res
 
 };
