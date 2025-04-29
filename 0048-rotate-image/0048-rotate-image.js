90d4b36cf5f1222aca2e1a8604eccaa33acdc94e
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-29 06:07:42 +0500

    Time: 0 ms (100%), Space: 54 MB (72.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,10 +3,26 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function (matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = i; j < matrix.length; j++) {
-            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+    // [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9]
+    // ]
 
+    // 1,1
+    // 1,2 -> 2.1
+    // 1.3 -> 3.1
+
+    // 1.2 -> 2.1
+    // 2.2 -> 2.2
+    // 2.3 -> 3.2
+
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix[0].length; j++) {
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
         }
         matrix[i] = matrix[i].reverse()
     }
