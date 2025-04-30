c74e4873d0cf90a5eef2f71ed78d3727444af9a0
Hassan Mehmood <ihassan463m@gmail.com>
2025-04-30 05:32:55 +0500

    Time: 17 ms (8.6%), Space: 56.7 MB (65.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -4,29 +4,51 @@
  * @return {number[][]}
  */
 var combinationSum2 = function (candidates, target) {
-    let set = new Set()
-    candidates = candidates.sort((a, v) => a - v)
-    var again = function (start, sum, arr) {
-        if (sum == target) {
-            let key = JSON.stringify(arr)
-            if (!set.has(key)) {
-                set.add(key)
-                return
-            }
-        }
-        let prev = -1
-        for (let i = start; i < candidates.length; i++) {
-            if (sum + candidates[i] <= target) {
-                if (prev == candidates[i]) continue
+    // let set = new Set()
+    // candidates = candidates.sort((a, v) => a - v)
+    // var again = function (start, sum, arr) {
+    //     if (sum == target) {
+    //         let key = JSON.stringify(arr)
+    //         if (!set.has(key)) {
+    //             set.add(key)
+    //             return
+    //         }
+    //     }
+    //     let prev = -1
+    //     for (let i = start; i < candidates.length; i++) {
+    //         if (sum + candidates[i] <= target) {
+    //             if (prev == candidates[i]) continue
 
-                if (sum + candidates[i] > target) break;
-                arr.push(candidates[i])
-                again(i + 1, sum + candidates[i], arr)
-                arr.pop()
-                prev = candidates[i]
-            }
+    //             if (sum + candidates[i] > target) break;
+    //             arr.push(candidates[i])
+    //             again(i + 1, sum + candidates[i], arr)
+    //             arr.pop()
+    //             prev = candidates[i]
+    //         }
+    //     }
+    // }
+    // again(0, 0, [])
+    // return Array.from(set).map(value => JSON.parse(value))
+
+
+    let set = new Set()
+    let res = []
+    candidates = candidates.sort((a, b) => a - b)
+    var again = function (index, arr, sum, set) {
+        let key = JSON.stringify(arr)
+        if (!set.has(key) && sum == target) {
+            res.push([...arr])
+            set.add(key)
+            return
+        }
+        if (sum > target) return
+        for (let i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) continue
+            arr.push(candidates[i])
+            again(i + 1, arr, sum + candidates[i], set)
+            arr.pop()
         }
     }
-    again(0, 0, [])
-    return Array.from(set).map(value => JSON.parse(value))
+    again(0, [], 0, set)
+    return res
 };
