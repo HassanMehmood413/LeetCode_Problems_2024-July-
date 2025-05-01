642ce1ea16f51f97fe208aeaed389efcae722bc7
Hassan Mehmood <ihassan463m@gmail.com>
2025-05-01 09:35:54 +0500

    Time: 42 ms (23.19%), Space: 82.8 MB (15.67%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -3,22 +3,40 @@
  * @return {string[][]}
  */
 var partition = function (s) {
-    let res = []
-    var again = function (start, arr) {
-        if (start == s.length) {
-            res.push([...arr])
-            return
-        }
+    // let res = []
+    // var again = function (start, arr) {
+    //     if (start == s.length) {
+    //         res.push([...arr])
+    //         return
+    //     }
 
-        for (let i = start; i < s.length; i++) {
-            let str = s.slice(start,i+1)
-            if (str.split('').reverse().join('') == str) {
+    //     for (let i = start; i < s.length; i++) {
+    //         let str = s.slice(start,i+1)
+    //         if (str.split('').reverse().join('') == str) {
+    //             arr.push(str)
+    //             again(i + 1,arr)
+    //             arr.pop()
+    //         }
+    //     }
+    // }
+    // again(0, [])
+    // return res
+
+    let res = []
+    var again = function (index, arr) {
+        if (index == s.length) {
+            res.push([...arr])
+        }
+        for (let i = index; i < s.length; i++) {
+            let str = s.slice(index, i + 1)
+            if (str.split("").reverse().join("") == str) {
                 arr.push(str)
-                again(i + 1,arr)
+                again(i + 1, arr)
                 arr.pop()
             }
         }
     }
     again(0, [])
     return res
+
 };
