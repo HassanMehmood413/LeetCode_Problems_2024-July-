f5b31f8192413fa46178c35fe9818f65587fb5bf
Hassan Mehmood <ihassan463m@gmail.com>
2025-05-01 09:05:26 +0500

    Time: 4 ms (67.67%), Space: 56.6 MB (73.64%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -31,26 +31,49 @@ var combinationSum2 = function (candidates, target) {
     // return Array.from(set).map(value => JSON.parse(value))
 
 
+    // let set = new Set()
+    // let res = []
+    // candidates = candidates.sort((a, b) => a - b)
+    // var again = function (index, arr, sum, set) {
+    //     let key = JSON.stringify(arr)
+    //     if (!set.has(key) && sum == target) {
+    //         res.push([...arr])
+    //         set.add(key)
+    //         return
+    //     }
+    //     if (sum > target) return
+    //     for (let i = index; i < candidates.length; i++) {
+    //         if (sum + candidates[i] <= target) {
+    //             if (i > index && candidates[i] == candidates[i - 1]) continue
+    //             arr.push(candidates[i])
+    //             again(i + 1, arr, sum + candidates[i], set)
+    //             arr.pop()
+    //         }
+    //     }
+    // }
+    // again(0, [], 0, set)
+    // return res
+
+
+
     let set = new Set()
     let res = []
-    candidates = candidates.sort((a, b) => a - b)
-    var again = function (index, arr, sum, set) {
+    candidates.sort((a, v) => a - v)
+    var again = function (index, arr, sum) {
         let key = JSON.stringify(arr)
-        if (!set.has(key) && sum == target) {
+        if (sum == target && !set.has(key)) {
             res.push([...arr])
             set.add(key)
-            return
         }
-        if (sum > target) return
         for (let i = index; i < candidates.length; i++) {
             if (sum + candidates[i] <= target) {
                 if (i > index && candidates[i] == candidates[i - 1]) continue
                 arr.push(candidates[i])
-                again(i + 1, arr, sum + candidates[i], set)
+                again(i + 1, arr, sum + candidates[i])
                 arr.pop()
             }
         }
     }
-    again(0, [], 0, set)
+    again(0, [], 0)
     return res
 };
