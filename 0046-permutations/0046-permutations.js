346bdb5186a2cc01937adb1d5531f8b6cdba5f7d
Hassan Mehmood <ihassan463m@gmail.com>
2025-05-06 04:33:09 +0500

    Time: 2 ms (66.01%), Space: 60.7 MB (13.22%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,20 +3,38 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
+    // let res = []
+    // var again = function (start, arr) {
+    //     if (arr.length == nums.length) {
+    //         res.push([...arr])
+    //         return
+    //     }
+    //     for (let i = 0; i < nums.length; i++) {
+    //         if (!arr.includes(nums[i])) {
+    //             arr.push(nums[i])
+    //             again(start + 1, arr)
+    //             arr.pop()
+    //         }
+    //     }
+    // }
+    // again(0, [])
+    // return res
+
+
     let res = []
-    var again = function (start, arr) {
+    var again = function (arr, used) {
         if (arr.length == nums.length) {
             res.push([...arr])
-            return
         }
         for (let i = 0; i < nums.length; i++) {
-            if (!arr.includes(nums[i])) {
-                arr.push(nums[i])
-                again(start + 1, arr)
-                arr.pop()
-            }
+            if (used.has(nums[i])) continue
+            used.add(nums[i])
+            arr.push(nums[i])
+            again(arr, used)
+            used.delete(nums[i])
+            arr.pop()
         }
     }
-    again(0, [])
+    again([], new Set())
     return res
 };
