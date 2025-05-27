554da9780b11ef35f427336f86c55c726e59fb0f
Hassan Mehmood <ihassan463m@gmail.com>
2025-05-27 05:14:06 +0500

    Time: 7 ms (10.68%), Space: 88.9 MB (11%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.js
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.js
@@ -13,37 +13,15 @@
  * @return {number}
  */
 var rangeSumBST = function (root, low, high) {
-    if(!root){
-        return 0
-    }
-    let queue = [root]
     let sum = 0
-    if(root.val >= low && root.val <=high){
-        sum = sum + root.val
-    }
-    while (queue.length > 0) {
-        let length = queue.length
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            if (first.left != null) {
-                queue.push(first.left)
-            }
-            if (first.right != null) {
-                queue.push(first.right)
-            }
-            if (first.left != null) {
-                if (first.left.val >= low && first.left.val <= high) {
-                    sum = sum + first.left.val
-                }
-            }
-            if (first.right != null) {
-
-                if (first.right.val >= low && first.right.val <= high) {
-                    sum = sum + first.right.val
-                }
-            }
+    var again = function (root) {
+        if (!root) return 0
+        if (root.val <= high && root.val >= low) {
+            sum += root.val
         }
+        again(root.left)
+        again(root.right)
     }
+    again(root)
     return sum
-
 };
