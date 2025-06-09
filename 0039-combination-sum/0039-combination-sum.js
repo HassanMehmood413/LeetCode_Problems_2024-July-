d93cbfa58e75eff1783e3689e3b3537b57c7fde6
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-09 07:28:38 +0500

    Time: 3 ms (67.96%), Space: 60.3 MB (39.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -4,33 +4,53 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
+    // // let res = []
+    // // var again = function (i, sum, arr) {
+    // //     if (sum == target) {
+    // //         res.push([...arr])
+    // //         return
+    // //     }
+
+    // //     for (let j = i; j < candidates.length; j++) {
+    // //         if (sum + candidates[j] <= target) {
+    // //             arr.push(candidates[j])
+    // //             again(j, sum + candidates[j], arr)
+    // //             arr.pop()
+    // //         }
+    // //     }
+    // // }
+    // // again(0, 0, [])
+    // // return res
+
+
     // let res = []
-    // var again = function (i, sum, arr) {
-    //     if (sum == target) {
+    // var again = function (index, arr, sum) {
+    //     if (target == sum) {
     //         res.push([...arr])
     //         return
     //     }
-
-    //     for (let j = i; j < candidates.length; j++) {
-    //         if (sum + candidates[j] <= target) {
-    //             arr.push(candidates[j])
-    //             again(j, sum + candidates[j], arr)
-    //             arr.pop()
-    //         }
+    //     if (sum > target) return
+    //     for (let i = index; i < candidates.length; i++) {
+    //         arr.push(candidates[i])
+    //         again(i, arr, sum + candidates[i])
+    //         arr.pop()
     //     }
     // }
-    // again(0, 0, [])
+    // again(0, [], 0)
     // return res
 
 
+
+
+
     let res = []
-    var again = function (index, arr, sum) {
-        if (target == sum) {
+    var again = function (start, arr, sum) {
+        if (sum == target) {
             res.push([...arr])
             return
         }
         if (sum > target) return
-        for (let i = index; i < candidates.length; i++) {
+        for (let i = start; i < candidates.length; i++) {
             arr.push(candidates[i])
             again(i, arr, sum + candidates[i])
             arr.pop()
