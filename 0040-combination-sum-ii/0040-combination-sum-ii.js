1fa6b625819b03322dea5feb535720eb7a1c3d13
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-10 08:18:09 +0500

    Time: 6 ms (39.53%), Space: 55.4 MB (98.34%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -56,18 +56,41 @@ var combinationSum2 = function (candidates, target) {
 
 
 
+    // let set = new Set()
+    // let res = []
+    // candidates.sort((a, v) => a - v)
+    // var again = function (index, arr, sum) {
+    //     let key = JSON.stringify(arr)
+    //     if (sum == target && !set.has(key)) {
+    //         res.push([...arr])
+    //         set.add(key)
+    //     }
+    //     for (let i = index; i < candidates.length; i++) {
+    //         if (sum + candidates[i] <= target) {
+    //             if (i > index && candidates[i] == candidates[i - 1]) continue
+    //             arr.push(candidates[i])
+    //             again(i + 1, arr, sum + candidates[i])
+    //             arr.pop()
+    //         }
+    //     }
+    // }
+    // again(0, [], 0)
+    // return res
+
+
+    candidates.sort((a, b) => a - b)
     let set = new Set()
     let res = []
-    candidates.sort((a, v) => a - v)
-    var again = function (index, arr, sum) {
+    var again = function (start, arr, sum) {
         let key = JSON.stringify(arr)
         if (sum == target && !set.has(key)) {
-            res.push([...arr])
             set.add(key)
+            res.push([...arr])
         }
-        for (let i = index; i < candidates.length; i++) {
+
+        for (let i = start; i < candidates.length; i++) {
             if (sum + candidates[i] <= target) {
-                if (i > index && candidates[i] == candidates[i - 1]) continue
+                if (i > start && candidates[i] == candidates[i - 1]) continue
                 arr.push(candidates[i])
                 again(i + 1, arr, sum + candidates[i])
                 arr.pop()
@@ -76,4 +99,6 @@ var combinationSum2 = function (candidates, target) {
     }
     again(0, [], 0)
     return res
+
+
 };
