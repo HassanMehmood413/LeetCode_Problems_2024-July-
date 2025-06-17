b11de77e8131a28d0f3930451e6ad349e896f0a3
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-17 23:18:03 +0500

    Time: 0 ms (100%), Space: 58 MB (13.17%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.js
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.js
@@ -12,26 +12,22 @@
  * @return {boolean}
  */
 var leafSimilar = function (root1, root2) {
-    // Helper function to perform DFS and collect leaf nodes
-    function dfs(root, leafList) {
-        if (root === null) {
-            return;
+    var dfs = function (root, leaflist) {
+        if (!root) return
+        if (!root.left && !root.right) {
+            leaflist.push(root.val)
         }
-
-        // Check if it is a leaf node
-        if (root.left === null && root.right === null) {
-            leafList.push(root.val);
+        if (root.left) {
+            dfs(root.left, leaflist)
         }
-
-        // Recursive DFS traversal
-        dfs(root.left, leafList);
-        dfs(root.right, leafList);
+        dfs(root.right, leaflist)
     }
 
-    const leaf1 = [];
-    const leaf2 = [];
+    let leaf1 = []
+    let leaf2 = []
 
-    dfs(root1, leaf1);
-    dfs(root2, leaf2);
-    return leaf1.join('_') == leaf2.join('_')
+    dfs(root1, leaf1)
+    dfs(root2, leaf2)
+    return leaf1.join(",") == leaf2.join(",")
+
 };
