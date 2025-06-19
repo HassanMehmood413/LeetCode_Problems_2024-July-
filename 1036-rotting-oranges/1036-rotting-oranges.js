275ab3ff0b08d44d24dfd783cc79863848414fc1
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-19 20:04:23 +0500

    Time: 13 ms (30.65%), Space: 57.2 MB (66.97%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.js
+++ 1036-rotting-oranges/1036-rotting-oranges.js
@@ -3,37 +3,41 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    let rows = grid.length
+    let cols = grid[0].length
+    let directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
     let queue = []
-    let fresh = 0
     let count = 0
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] == 2) {
-                queue.push([i, j])
+    let fresh = 0
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] == 2) {
+                queue.push([r, c])
             }
-            else if (grid[i][j] == 1) {
+            else if (grid[r][c] == 1) {
                 fresh++
             }
         }
     }
+
     while (queue.length > 0 && fresh > 0) {
         let length = queue.length
         for (let i = 0; i < length; i++) {
             let [first, second] = queue.shift()
-            for (let [x, y] of [[-1, 0], [0, -1], [1, 0], [0, 1]]) {
-                let a = x + first
-                let b = y + second
+            for (let [row, col] of directions) {
+                let newr = row + first
+                let newc = col + second
 
-                if (a >= 0 && b >= 0 && a < grid.length && b < grid[0].length && grid[a][b] == 1) {
-                    grid[a][b] = 0
-                    queue.push([a, b])
+                if (newr >= 0 && newr < rows && newc < grid[0].length && newr < grid.length && grid[newr][newc] == 1) {
+                    queue.push([newr,newc])
+                    grid[newr][newc] = 0
                     fresh--
                 }
             }
         }
         count++
     }
-
-
     return fresh == 0 ? count : -1
+
 };
