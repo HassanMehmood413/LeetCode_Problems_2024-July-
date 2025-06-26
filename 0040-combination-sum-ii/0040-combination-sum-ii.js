d2eb43f89fa314991ba4256cf759b81d6ffc61c9
Hassan Mehmood <ihassan463m@gmail.com>
2025-06-26 22:08:13 +0500

    Time: 5 ms (48.24%), Space: 55.9 MB (92.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -3,6 +3,27 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum2 = function(candidates, target) {
-    
+var combinationSum2 = function (candidates, target) {
+    candidates.sort((a, b) => a - b)
+    let set = new Set()
+    let res = []
+    var again = function (start, arr, sum) {
+        let key = JSON.stringify(arr)
+        if (sum == target && !set.has(key)) {
+            set.add(key)
+            res.push([...arr])
+        }
+
+        for (let i = start; i < candidates.length; i++) {
+            if (sum + candidates[i] <= target) {
+                if (i > start && candidates[i] == candidates[i - 1]) continue
+                arr.push(candidates[i])
+                again(i + 1, arr, sum + candidates[i])
+                arr.pop()
+            }
+        }
+    }
+    again(0, [], 0)
+    return res
+
 };
