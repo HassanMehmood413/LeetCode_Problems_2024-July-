6c4df47d8fb6cdded973d22e172e14811248f5be
Hassan Mehmood <ihassan463m@gmail.com>
2025-07-03 07:50:13 +0500

    Time: 10 ms (68.84%), Space: 58.9 MB (97.95%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,42 +4,6 @@
  * @return {number[]}
  */
 var findOrder = function (numCourses, prerequisites) {
-    // let graph = new Map()
-    // let result = []
-    // let indegree = new Array(numCourses).fill(0)
-    // let queue = []
-    // var again = function (graph, first, second) {
-    //     if (!graph.has(first)) {
-    //         graph.set(first, [])
-    //     }
-    //     graph.get(first).push(second)
-    //     indegree[second]++
-    // }
-
-    // for (let i = 0; i < prerequisites.length; i++) {
-    //     again(graph, prerequisites[i][0], prerequisites[i][1])
-    // }
-
-
-    // for (let i = 0; i < numCourses; i++) {
-    //     if (indegree[i] == 0) {
-    //         queue.push(i)
-    //     }
-    // }
-
-    // while (queue.length > 0) {
-    //     let first = queue.shift()
-    //     result.push(first)
-    //     for (let neighbor of graph.get(first) || []) {
-    //         indegree[neighbor]--
-    //         if (indegree[neighbor] == 0) {
-    //             queue.push(neighbor)
-    //         }
-    //     }
-    // }
-    // return result.length == numCourses ? result.reverse() : []
-
-
     let graph = new Map()
     let result = []
     let indegree = new Array(numCourses).fill(0)
@@ -73,5 +37,4 @@ var findOrder = function (numCourses, prerequisites) {
         }
     }
     return result.length == numCourses ? result.reverse() : []
-
 };
