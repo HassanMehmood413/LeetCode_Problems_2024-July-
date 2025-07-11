67e760cc8e6df60436b3989c03c743ffaafe7f91
Hassan Mehmood <ihassan463m@gmail.com>
2025-07-11 09:41:41 +0500

    Time: 2 ms (85.96%), Space: 61.3 MB (63.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,19 +11,20 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    let dummy = new ListNode()
-    let prev = dummy
     let carry = 0
+    let dummy = new ListNode()
+    let temp = dummy
+
     while (l1 || l2 || carry) {
-        v1 = l1 ? l1.val : 0
-        v2 = l2 ? l2.val : 0
-        sum = v1 + v2 + carry
+        list1 = l1? l1.val : 0
+        list2 = l2? l2.val : 0
+        sum = list1 + list2 + carry
         carry = Math.floor(sum / 10)
         sum = sum % 10
-        prev.next = new ListNode(sum)
-        prev = prev.next
-        l1 = l1 ? l1.next : null
-        l2 = l2 ? l2.next : null
+        temp.next = new ListNode(sum)
+        temp = temp.next
+        l1 = l1? l1.next : null
+        l2 = l2? l2.next : null
     }
     return dummy.next
 };
