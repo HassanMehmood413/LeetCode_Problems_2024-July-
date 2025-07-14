e73e517297aee3d00c2f232bd4d80c4e4cde8a23
Hassan Mehmood <ihassan463m@gmail.com>
2025-07-14 19:45:57 +0500

    Time: 4 ms (55.45%), Space: 55.2 MB (40.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,7 +5,7 @@
  */
 var twoSum = function (nums, target) {
     let map = new Map()
-    for(let i = 0;i<nums.length;i++){
+    for (let i = 0; i < nums.length; i++) {
         let a = target - nums[i]
         if (map.has(a)) {
             return [map.get(a), i]
