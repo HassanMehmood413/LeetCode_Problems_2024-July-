158e41c8999278778c5b575802611007e5bff15e
Hassan Mehmood <ihassan463m@gmail.com>
2025-07-21 01:45:53 +0500

    Time: 24 ms (34.3%), Space: 77.5 MB (16.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,23 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    // let map = new Map()
-    // nums.forEach((value) => {
-    //     if (map.has(value)) {
-    //         map.set(value, map.get(value) + 1)
-    //     }
-    //     else {
-    //         map.set(value, 1)
-    //     }
-    // })
-    // for (let [value, index] of map) {
-    //     if (index > 1) return true
-    // }
-    // return false
-
     let set = new Set()
-    for (let i = 0; i < nums.length; i++){
-        if(set.has(`${nums[i]}`)){
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(`${nums[i]}`)) {
             return true
         }
         set.add(`${nums[i]}`)
