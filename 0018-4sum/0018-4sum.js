22d1bdfb40a7d997bd4ce3ba14c937ed1da59f6c
Hassan Mehmood <ihassan463m@gmail.com>
2025-08-19 09:47:12 +0500

    Time: 19 ms (35.24%), Space: 57.4 MB (47.69%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -5,39 +5,39 @@
  */
 var fourSum = function (nums, target) {
     nums = nums.sort((a, b) => a - b)
-    let temp = []
-    let sum = 0
-    for (let a = 0; a < nums.length - 3; a++) {
-        if (a > 0 && nums[a] == nums[a - 1]) {
-            continue
-        }
-        for (let b = a + 1; b < nums.length - 2; b++) {
-            if (b > a + 1 && nums[b] == nums[b - 1]) {
-                continue
-            }
+    let result = []
+
+    for (let a = 0; a < nums.length; a++) {
+        if (a > 0 && nums[a] == nums[a - 1]) continue
+
+        for (let b = a + 1; b < nums.length; b++) {
+            if (b > a + 1 && nums[b] == nums[b - 1]) continue
             let c = b + 1
             let d = nums.length - 1
+
             while (c < d) {
-                sum = nums[a] + nums[b] + nums[c] + nums[d]
-                if (sum < target) {
-                    c++
-                }
-                else if (sum > target) {
+                let sum = nums[a] + nums[b] + nums[c] + nums[d]
+                if (sum > target) {
                     d--
+                }
+                else if (sum < target) {
+                    c++
                 }
                 else {
-                    temp.push([nums[a], nums[b], nums[c], nums[d]])
-                    while (c < d && nums[c] == nums[c + 1]) {
-                        c++
-                    }
-                    while (c < d && nums[d] == nums[d - 1]) {
-                        d--
-                    }
+
+                    result.push([nums[a], nums[b], nums[c], nums[d]])
                     c++
                     d--
+                    while (c < d && nums[c] == nums[c - 1]) {
+                        c++
+                    }
+
+                    while (c < d && nums[d] == nums[d + 1]) {
+                        d--
+                    }
                 }
             }
         }
     }
-    return temp
-}
+    return result
+};
