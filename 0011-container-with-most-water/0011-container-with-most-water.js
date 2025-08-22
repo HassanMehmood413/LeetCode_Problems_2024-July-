f36d6574254bd743f65d9cbcf1247f4b6006151e
Hassan Mehmood <ihassan463m@gmail.com>
2025-08-22 07:41:45 +0500

    Time: 3 ms (54.6%), Space: 61.4 MB (92.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,37 +3,36 @@
  * @return {number}
  */
 var maxArea = function (height) {
-    // let left = 0
-    // let right = height.length - 1
-    // let max = 0
-    // while(left<right){
-    //     Area = (right-left) * Math.min(height[left],height[right])
-    //     max = Math.max(max,Area)
-    //     if(height[left]<height[right]){
-    //         left++
+    // let l = 0
+    // let w = 0
+    // arr = []
+    // res = 0
+    // for (let i = 0; i < height.length; i++) {
+    //     if (height[i] > l) {
+    //         l = Math.max(l, height[i])
+    //         arr.push([l, i])
     //     }
-    //     else{
-    //         right--
+    //     w = Math.abs(arr[arr.length - 1][1] - i)
+    //     let value_at_index = height[i]
+    //     let area = (l - (l - value_at_index)) * w
+    //     if (area > res) {
+    //         res = area
     //     }
     // }
-    // return max
+    // return res
 
-
-
-
-    let left = 0
-    let right = height.length - 1
+    let l = 0
+    let r = height.length - 1
     let res = 0
-    while(left < right){
-        area = (right - left) * Math.min(height[left],height[right])
-        res = Math.max(res,area)
-        if(height[left] < height[right]){
-            left++
+    while (l < r) {
+        let h = Math.min(height[l], height[r])
+        res = Math.max(res, h * (r - l))
+        if (height[l] <= height[r]) {
+            l++
         }
-        else{
-            right--
+        else {
+            r--
         }
     }
     return res
-
 };
