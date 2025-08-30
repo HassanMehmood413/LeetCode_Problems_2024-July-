8577f7903133addb5032ebbe28afbdb376b63838
Hassan Mehmood <ihassan463m@gmail.com>
2025-08-30 08:14:04 +0500

    Time: 2 ms (81.85%), Space: 62.4 MB (68.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,24 +3,6 @@
  * @return {number}
  */
 var maxArea = function (height) {
-    // let l = 0
-    // let w = 0
-    // arr = []
-    // res = 0
-    // for (let i = 0; i < height.length; i++) {
-    //     if (height[i] > l) {
-    //         l = Math.max(l, height[i])
-    //         arr.push([l, i])
-    //     }
-    //     w = Math.abs(arr[arr.length - 1][1] - i)
-    //     let value_at_index = height[i]
-    //     let area = (l - (l - value_at_index)) * w
-    //     if (area > res) {
-    //         res = area
-    //     }
-    // }
-    // return res
-
     let l = 0
     let r = height.length - 1
     let res = 0
