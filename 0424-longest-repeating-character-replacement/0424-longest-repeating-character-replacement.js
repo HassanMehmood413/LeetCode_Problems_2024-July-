1aa8469e1f19fc7c23da048b66632a092360e129
Hassan Mehmood <ihassan463m@gmail.com>
2025-09-03 08:54:58 +0500

    Time: 13 ms (88.02%), Space: 55.7 MB (83.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,28 +4,29 @@
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-    let max = 0
-    let length = s.length
-    for (let right = 65; right <= 90; right++) {
-        let i = 0; j = 0; replace = 0
-        while (j < length) {
-            if (s[j] == String.fromCharCode(right)) {
-                j++
-            }
-            else if (replace < k) {
-                replace++
-                j++
-            }
-            else if (s[i] == String.fromCharCode(right)) {
-                i++
-            }
-            else {
-                replace--
-                i++
-            }
-            max = Math.max(max, j - i)
-        }
-    }
-    return max
+    // current length - most repeated current number 
+    let most = 0;
+    let ans = 0;
+    let left = 0;
+    let freq = new Map();
 
-}
+    for (let right = 0; right < s.length; right++) {
+        let ch = s[right];
+
+        let newCount = (freq.get(ch) || 0) + 1;
+        freq.set(ch, newCount);
+
+        most = Math.max(most, newCount);
+
+        while ((right - left + 1) - most > k) {
+            let leftCh = s[left++];
+            let leftCount = (freq.get(leftCh) || 0) - 1;
+            if (leftCount > 0) freq.set(leftCh, leftCount);
+            else freq.delete(leftCh);
+        }
+
+        ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+};
