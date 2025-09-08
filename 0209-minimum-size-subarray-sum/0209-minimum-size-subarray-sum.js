34873db136bc99a7555afd933d4dbfeae08fba71
Hassan Mehmood <ihassan463m@gmail.com>
2025-09-08 07:12:46 +0500

    Time: 230 ms (5.01%), Space: 63.1 MB (5.44%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,27 +4,21 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
+    let arr = []
     let left = 0
-    let sum = nums[0]
-    let length = 1
-    let min = Infinity
-    for (let right = 1; right <= nums.length; right++) {
-        if (sum >= target) {
-            while (sum >= target) {
-                min = Math.min(min, length)
-                sum = sum - nums[left]
-                left++
-                length--
-            }
-        }
-        sum = sum + nums[right]
-        length++
+    let sum = 0
+    let max_sub = Infinity
 
+    for (let right = 0; right < nums.length; right++) {
+
+        arr.push(nums[right])
+        sum = sum + nums[right]
+        while (arr.length > 0 && sum >= target) {
+            max_sub = Math.min(max_sub, arr.length)
+            sum = sum - nums[left]
+            arr.shift()
+            left++
+        }
     }
-    if (min == Infinity) {
-        return 0
-    }
-    else {
-        return min
-    }
+    return max_sub == Infinity ? 0 : max_sub
 };
