160e9bade5f054a63a0e6f8065720307c7b046e7
Hassan Mehmood <ihassan463m@gmail.com>
2025-09-18 03:16:25 +0500

    Time: 0 ms (100%), Space: 54 MB (49.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,16 +5,20 @@
  */
 var searchInsert = function (nums, target) {
     let left = 0
-    let right = nums.length
-    while (left < right) {
+    let right = nums.length - 1
+
+    while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (nums[mid] < target) {
+
+        if (nums[mid] == target) {
+            return mid
+        }
+        else if (nums[mid] < target) {
             left = mid + 1
         }
-        else {
-            right = mid
+        else if (nums[mid] > target) {
+            right = mid - 1
         }
     }
     return left
-
 };
