741d6c3225da9a2603c12ae3206cd1191162d91a
Hassan Mehmood <ihassan463m@gmail.com>
2025-09-24 07:19:37 +0500

    Time: 94 ms (5.16%), Space: 66 MB (5.1%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.js
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.js
@@ -4,16 +4,22 @@
  * @return {number}
  */
 var minEatingSpeed = function (piles, h) {
-    let max = Math.max(...piles)
-    let ultra = Infinity
     let left = 1
-    let right = max
+    let right = Math.max(...piles)
+    let ultra = Infinity
+
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
         let count = 0
+        console.log(mid)
+
         for (let i = 0; i < piles.length; i++) {
             count += Math.ceil(piles[i] / mid)
         }
+        console.log("----------------")
+        console.log(count)
+
+
         if (count <= h) {
             ultra = mid
             right = mid - 1
@@ -21,6 +27,7 @@ var minEatingSpeed = function (piles, h) {
         else {
             left = mid + 1
         }
+
     }
     return ultra
 };
