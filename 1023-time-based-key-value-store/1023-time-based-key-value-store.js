67c659b4643d980e91698103cbceb608e820ca76
Hassan Mehmood <ihassan463m@gmail.com>
2025-09-28 08:38:23 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.js
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.js
@@ -1,6 +1,6 @@
 
 var TimeMap = function () {
-    this.map = new Map()
+    this.store = new Map()
 };
 
 /** 
@@ -10,10 +10,12 @@ var TimeMap = function () {
  * @return {void}
  */
 TimeMap.prototype.set = function (key, value, timestamp) {
-    if (!this.map.has(key)) {
-        this.map.set(key, [])
+    console.log("SET")
+    if (!this.store.get(key)) {
+        this.store.set(key, [])
     }
-    this.map.get(key).push([value, timestamp])
+    this.store.get(key).push([value, timestamp])
+    console.log(this.store)
 };
 
 /** 
@@ -22,22 +24,28 @@ TimeMap.prototype.set = function (key, value, timestamp) {
  * @return {string}
  */
 TimeMap.prototype.get = function (key, timestamp) {
-    let res = ''
-    if(!this.map.has(key)) return res
+    console.log("GET")
+    let arr = this.store.get(key)
     let left = 0
-    let values = this.map.get(key, [])
-    let right = values.length - 1
+    let right = arr.length - 1
+    let ans = ""
+
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (values[mid][1] <= timestamp) {
-            res = values[mid][0]
+        let [v, t] = arr[mid]
+        console.log(v, t)
+
+        if (t <= timestamp) {
+            ans = v
             left = mid + 1
         }
         else {
             right = mid - 1
         }
     }
-    return res
+    return ans
+
+
 };
 
 /** 
