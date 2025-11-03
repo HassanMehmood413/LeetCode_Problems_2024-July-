a848aa02304b92c489c90652d7a679af3dd9f879
Hassan Mehmood <ihassan463m@gmail.com>
2025-11-03 09:04:24 +0500

    Time: 0 ms (100%), Space: 58 MB (54.95%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,25 +11,33 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    let queue = [root]
+    // let queue = [root]
+    // if (!root) return 0
+    // if (root && !root.left && !root.right) return 1
+
+    // let depth = 0
+
+    // while (queue.length > 0) {
+    //     let length = queue.length
+
+    //     for (let i = 0; i < length; i++) {
+    //         let first = queue.shift()
+    //         if (first.left != null) {
+    //             queue.push(first.left)
+    //         }
+    //         if (first.right != null) {
+    //             queue.push(first.right)
+    //         }
+    //     }
+    //     depth++
+    // }
+    // return depth
+
+
+    // recursion technique 
     if (!root) return 0
-    if (root && !root.left && !root.right) return 1
+    let left = maxDepth(root.left) + 1
+    let right = maxDepth(root.right) + 1
+    return Math.max(left, right)
 
-    let depth = 0
-
-    while (queue.length > 0) {
-        let length = queue.length
-
-        for (let i = 0; i < length; i++) {
-            let first = queue.shift()
-            if (first.left != null) {
-                queue.push(first.left)
-            }
-            if (first.right != null) {
-                queue.push(first.right)
-            }
-        }
-        depth++
-    }
-    return depth
 };
