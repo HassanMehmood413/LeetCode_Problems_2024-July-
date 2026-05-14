d55a7bc71227bd789623d79dd2b6fd6ae3c7e0f7
Hassan Mehmood <ihassan463m@gmail.com>
2026-05-14 21:27:14 +0500

    Time: 17 ms (65.24%), Space: 80 MB (6.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -7,15 +7,27 @@ var containsDuplicate = function (nums) {
 
     // cannot use loop, 
     // cannot use include, it also checks whole array
-    // sort > 0n(n-1)
-    dup_chk = dup_chk.sort((a, b) => a - b)
-    console.log(dup_chk)
+    // sort > n(log n) with o1 space
+    // dup_chk = dup_chk.sort((a, b) => a - b)
+    // console.log(dup_chk)
 
-    for (let i = 0; i < dup_chk.length - 1; i++) {
-        if (dup_chk[i] == dup_chk[i + 1]) {
+    // for (let i = 0; i < dup_chk.length - 1; i++) {
+    //     if (dup_chk[i] == dup_chk[i + 1]) {
+    //         return true
+    //     }
+    // }
+    // return false
+
+    // 2. in case of using hashmap
+    let dup_map = new Map()
+
+    for (let value of nums) {
+        if (dup_map.has(value)) {
             return true
         }
+        dup_map.set(value, 1)
     }
+
     return false
 
 
