f8ba85b76426f2de876253acd69857278abb1815
Hassan Mehmood <ihassan463m@gmail.com>
2026-05-14 21:08:16 +0500

    Time: 85 ms (5.31%), Space: 67 MB (93.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,12 +3,20 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    let set = new Set()
-    for (let i = 0; i < nums.length; i++) {
-        if (set.has(`${nums[i]}`)) {
+    let dup_chk = nums
+
+    // cannot use loop, 
+    // cannot use include, it also checks whole array
+    // sort > 0n(n-1)
+    dup_chk = dup_chk.sort((a, b) => a - b)
+    console.log(dup_chk)
+
+    for (let i = 0; i < dup_chk.length - 1; i++) {
+        if (dup_chk[i] == dup_chk[i + 1]) {
             return true
         }
-        set.add(`${nums[i]}`)
     }
     return false
+
+
 };
