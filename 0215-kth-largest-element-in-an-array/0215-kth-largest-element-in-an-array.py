b5f2b2ea7f5df9f8b346eb230b3be6ba1a3c37c9
Hassan Mehmood <ihassan463m@gmail.com>
2026-07-24 13:57:32 +0500

    Time: 87 ms (63.33%), Space: 31.2 MB (20.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,10 +1,33 @@
+import random
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pq = nums[:k]
-        heapq.heapify(pq)
+        arr = nums
+        target = len(arr) - k
 
-        for i in range(k, len(nums)):
-            if nums[i] > pq[0]:
-                heapq.heappop(pq)
-                heapq.heappush(pq, nums[i])
-        return heapq.heappop(pq)
+        low = 0
+        high = len(arr) - 1
+        while True:
+            pivot = arr[random.randint(low, high)]
+
+            lt = low        # start of the "equal" pile
+            i = low         # the one doing the looking
+            gt = high       # end of the "equal" pile
+
+            while i <= gt:
+                if arr[i] < pivot:
+                    arr[lt], arr[i] = arr[i], arr[lt]
+                    lt += 1
+                    i += 1
+                elif arr[i] > pivot:
+                    arr[i], arr[gt] = arr[gt], arr[i]
+                    gt -= 1
+                else:
+                    i += 1
+
+            if target < lt:
+                high = lt - 1
+            elif target > gt:
+                low = gt + 1
+            else:
+                return pivot
