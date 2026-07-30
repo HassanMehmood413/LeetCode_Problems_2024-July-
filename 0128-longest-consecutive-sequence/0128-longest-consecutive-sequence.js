60f30c7d69422e191a1c7dde1f9f13c8be91b6fd
Hassan Mehmood <ihassan463m@gmail.com>
2026-07-30 17:20:40 +0500

    Time: 35 ms (83.49%), Space: 81 MB (35.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,18 +3,23 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    let set = new Set(nums)
-    let max = 0
-    for (let i = 0; i < nums.length; i++) {
-        if (!set.has(nums[i] - 1)) {
-            let count = 1
-            num = nums[i]
-            while (set.has(num + 1)) {
-                num = num + 1
-                count++
-            }
-            max = Math.max(max, count)
+    if (nums.length == 0) return 0
+    let nums_set = new Set(nums)
+    let max_count = 0
+    for (let i of nums_set) {
+        if (!nums_set.has(i - 1)) {
+            max_count = Math.max(max_count, check_count(i, nums_set));
         }
     }
-    return max
+    return max_count
 };
+
+var check_count = function (num, nums_set) {
+    let count = 0
+    while (nums_set.has(num)) {
+        num = num + 1
+        count++
+    }
+    return count
+
+}
