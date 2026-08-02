e3839a15896714dc16f943e835110439dc9f6361
Hassan Mehmood <ihassan463m@gmail.com>
2026-08-02 09:39:15 +0500

    Time: 1 ms (61.68%), Space: 59.6 MB (92.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,18 +2,6 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function (s) {
-    let first = 0
-    let end = s.length - 1
-
-    while (first < end) {
-        let temp = s[first]
-        s[first] = s[end]
-        s[end] = temp
-
-        first++
-        end--
-    }
-    return s
-
+var reverseString = function(s) {
+    return s.reverse()
 };
